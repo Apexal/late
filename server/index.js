caeb4507ccad0c83adeac20ee98ce4f115d1a7b2
Frank Matranga <thefrankmatranga@gmail.com>
2018-09-23 18:28:14 -0400

    Request logger

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 /* Koa dependencies */
 const Koa = require('koa');
 const Router = require('koa-router');
+const Logger = require('koa-logger');
 
 const app = new Koa();
 const router = new Router();
+
+app.use(Logger());
 
 app.use(async (ctx, next) => {
   /* This is run before every single request is handled specifically. */
