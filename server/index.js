b46294c9af8cf29bbee7ea38f7520c2c24b5bddd
Frank Matranga <thefrankmatranga@gmail.com>
2018-09-24 17:13:52 -0400

    Session setup

--- server/index.js
+++ server/index.js
@@ -4,9 +4,19 @@ const Router = require('koa-router');
 const Logger = require('koa-logger');
 const Static = require('koa-static');
 const Helmet = require('koa-helmet');
+const Session = require('koa-session');
 
 const app = new Koa();
 const router = new Router();
+
+app.keys = ['WE ARE GOING TO CHANGE THIS'];
+
+/* Setup session */
+const CONFIG = {
+  key: 'koa:sess',
+  maxAge: 86400000
+};
+app.use(Session(CONFIG, app));
 
 /* Better security by default */
 app.use(Helmet());
@@ -19,6 +29,7 @@ app.use(Static('client/public'));
 
 app.use(async (ctx, next) => {
   /* This is run before every single request is handled specifically. */
+
   await next();
 });
 
