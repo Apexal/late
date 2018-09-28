52a4e8c9709f504fb48d858451849caae5ae9a96
Frank Matranga <thefrankmatranga@gmail.com>
2018-09-28 16:54:07 -0400

    View preparation

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const Static = require('koa-static');
 const Helmet = require('koa-helmet');
 const Session = require('koa-session');
 const KoaBody = require('koa-body');
+const Views = require('koa-views');
 
 const app = new Koa();
 const router = new Router();
@@ -29,6 +30,18 @@ app.use(Logger());
 
 /* Serve static files (CSS, JS, audio, etc.) */
 app.use(Static('client/public'));
+
+/* Setup view system */
+// TODO: UNCOMMENT WHEN TEMPLATING LANGUAGE IS CHOSEN
+/*
+app.use(
+  Views(__dirname + '/views', {
+    map: {
+      html: 'underscore'
+    }
+  })
+);
+*/
 
 app.use(async (ctx, next) => {
   /* This is run before every single request is handled specifically. */
