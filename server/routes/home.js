3ced373482ec57ccc1953594f448c25492d72117
Frank Matranga <thefrankmatranga@gmail.com>
2018-10-12 22:08:12 -0400

    Improved auth - /authenticate -> /login - set default redirectTo - hero links

--- server/routes/home.js
+++ server/routes/home.js
@@ -9,7 +9,17 @@ const Ctrl = require('../controllers/home');
 router.get('/', Ctrl.index);
 router.get('about', Ctrl.about);
 router.get('honorable', cas.bounce, Ctrl.honorable);
-router.get('authenticate', cas.bounce_redirect);
+
+// CAS Auth routes
+router.get(
+  'login',
+  async (ctx, next) => {
+    // In case just /login without redirectTo in the query
+    ctx.query.redirectTo = ctx.query.redirectTo || '/';
+    await next();
+  },
+  cas.bounce_redirect
+);
 router.get('logout', cas.logout);
 
 module.exports = router.routes();
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,3 @@
-const cas = require('../auth.js');
-
 module.exports = router => {
   // router.user(path, router);
   router.use('/', require('./home'));
