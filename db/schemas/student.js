51a4b1e62e591664d6b21d1734a63b4100ec376f
Frank Matranga <thefrankmatranga@gmail.com>
2018-10-14 00:37:11 -0400

    Student schema start

---
+++ db/schemas/student.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const rpiValidator = require('rpi-validator');
+
+const schema = new Schema({
+  rin: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(rin) {
+        return rpiValidator.isRIN(rin);
+      },
+      message: props => `${props.value} is not a valid RIN!`
+    }
+  },
+  name: {
+    first: { type: String, required: true },
+    preferred: { type: String, required: true },
+    last: { type: String, required: true }
+  },
+  rcs_id: { type: String, required: true },
+  grad_year: { type: Number, required: true }, // maybe?
+  admin: { type: Boolean, default: false },
+  joined_date: { type: Date, required: true },
+  last_login: Date
+});
+
+module.exports = {
+  name: 'Student',
+  schema
+};
