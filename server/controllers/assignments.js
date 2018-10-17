fa4162f91dcb02b4dd3cdd005619813f82cdfd05
Chris Nero <chrisvn00@gmail.com>
2018-10-17 17:48:07 -0400

    Kind of build an assignment

--- server/controllers/assignments.js
+++ server/controllers/assignments.js
@@ -1,10 +1,24 @@
+const db = require('../../db');
+
 async function getNew(ctx, next) {
   ctx.state.title = "New Assignment";
   await ctx.render('assignments/new');
 }
 
 async function postNew(ctx, next) {
-  // TODO
+  const body = ctx.request.body;
+  console.log(body);
+  // TODO validation
+  const Assignment = db.models.Assignment;
+  const assign = new Assignment({
+    title: body.title,
+    description: body.description,
+    dueDate: new Date(body.due_date),
+    timeEstimate: parseInt(body.time_estimate, 10),
+    timeRemaining: parseInt(body.time_estimate, 10),
+    priority: parseInt(body.priority)
+  });
+  // TODO save assignment to database
   ctx.redirect('/');
 }
 
