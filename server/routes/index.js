0f95466ecc610444c40c5923dcde6714feb506f1
Frank Matranga <thefrankmatranga@gmail.com>
2018-10-19 14:28:01 -0400

    Authenticate routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
+const cas = require('../auth').cas;
+
 module.exports = router => {
   // router.user(path, router);
-  router.use('/api', require('./api'));
-  router.use('/assignments', require('./assignments'));
+  router.use('/api', cas.bounce, require('./api'));
+  router.use('/assignments', cas.bounce, require('./assignments'));
   router.use('/', require('./home'));
 };
