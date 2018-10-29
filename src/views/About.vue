c73ee512b342176b5b06716645a9e2a7f135802b
Frank Matranga <thefrankmatranga@gmail.com>
2018-10-29 16:24:03 -0400

    Improving starting views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="about">
+    <section class="section">
+      <div class="container">
+        <div id="goal">
+          <div class="content">
+            <h1 class="title is-4">The Goal</h1>
+            <p>To provide the user with a clear list of all upcoming assignments and tests.</p>
+            <p>To automatically allocate free time in a user’s calendar to work on/study for assignments/tests.</p>
+            <p>To remind users to follow the allocated study/work times through means including notifications, text messages, etc.</p>
+            <p>To account for missing scheduled work by reallocating work times</p>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div id="team">
+          <div class="content">
+            <h1 class="title is-4">The Team</h1>
+            <h4 class="subtitle">Project Contributors</h4>
+            <p>Frank Matranga Zach Love Joseph Cengel</p>
+            <p>Shweta Burgula Abigail Medina Kikola Sanusi</p>
+            <p>Seth Laurenceau Zachary Fernandez Tyler Machado</p>
+            <p>Ziyi Wang Seungwon Kim Christie Nero</p>
+            <p>Bowen Wang</p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
