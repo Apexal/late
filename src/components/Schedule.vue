c2a4df6143a48c2fd39b3fa8580f2adb1b507663
Frank Matranga <thefrankmatranga@gmail.com>
2018-11-01 17:47:22 -0400

    Schedule module shows current info (start)

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -2,6 +2,12 @@
   <div class="schedule">
     <div class="box">
       <h2 class="subtitle">Your Schedule for {{ dateStr }}</h2>
+      <template v-if="schedule.in_class">
+        <span class="icon">
+          <i class="fas fa-chalkboard-teacher"/>
+        </span>
+        <b>{{ schedule.current_period.longname }} {{ currentPeriodType }}</b>
+      </template>
     </div>
   </div>
 </template>
@@ -18,7 +24,18 @@ export default {
   },
   computed: {
     schedule () { return this.$store.state.schedule; },
-    dateStr () { return moment(this.schedule.date).format('YYYY-MM-DD'); }
+    dateStr () { return moment(this.schedule.date).format('YYYY-MM-DD'); },
+    currentPeriodType () {
+      if (!this.schedule.in_class) return;
+      switch(this.schedule.current_period.type) {
+      case 'TES':
+        return 'Test';
+      case 'LEC':
+        return 'Lecture';
+      default:
+        return 'Class';
+      }
+    }
   }
 };
 </script>
