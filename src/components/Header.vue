2274c0254d663154eaaac417c74dd32b4ab030ed
Frank Matranga <thefrankmatranga@gmail.com>
2018-11-02 21:30:03 -0400

    Header real navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,25 +8,80 @@
         </div>
       </div>
 
-      <div class="hero-foot">
-        <nav class="tabs">
-          <div class="container">
-            <ul>
-              <template v-if="loggedIn">
-                <li>
-                  <a>Logged in as <b class="rcs_id">{{ user.rcs_id }}</b></a>
-                </li>
-                <li>
-                  <a href="/auth/logout">Logout</a>
-                </li>
-              </template>
-              <li v-else><a href="/auth/login">Login</a></li>
-            </ul>
-          </div>
-        </nav>
-      </div>
+
     </section>
+    <div class="hero-foot">
+      <nav
+        class="navbar is-dark"
+        role="navigation"
+        aria-label="main navigation">
+        <div class="navbar-brand">
+          <a
+            role="button"
+            class="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="top-navbar">
+            <span aria-hidden="true"/>
+            <span aria-hidden="true"/>
+            <span aria-hidden="true"/>
+          </a>
+        </div>
+
+        <div
+          id="top-navbar"
+          class="navbar-menu container">
+          <div class="navbar-start">
+            <li class="navbar-item">
+              <router-link to="/dashboard">Dashboard</router-link>
+            </li>
+
+            <li class="navbar-item">
+              <router-link to="/about">About</router-link>
+            </li>
+
+            <!--
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                More
+              </a>
+
+
+              <div class="navbar-dropdown">
+                <a class="navbar-item">
+                  Dropdown
+                </a>
+
+                <a class="navbar-item">
+                  Contact
+                </a>
+                <hr class="navbar-divider">
+                <a class="navbar-item">
+                  Report an issue
+                </a>
+              </div>
+            </div>
+            -->
+          </div>
+
+          <div class="navbar-end">
+            <template v-if="loggedIn">
+              <a class="navbar-item">Logged in as <b class="rcs_id">{{ user.rcs_id }}</b></a>
+              <a
+                class="navbar-item"
+                href="/auth/logout">Logout</a>
+            </template>
+            <a
+              v-else
+              class="navbar-item"
+              href="/auth/login"><b>Login</b></a>
+
+          </div>
+        </div>
+      </nav>
+    </div>
   </header>
+
 </template>
 
 <script>
@@ -46,5 +101,20 @@ export default {
 <style lang="scss" scoped>
 .rcs_id {
   margin-left: 5px;
+}
+
+#top-navbar {
+  li.navbar-item {
+    a {
+      color: inherit;
+    }
+
+    &:hover {
+      a {
+        color: black;
+      }
+      background-color: white;
+    }
+  }
 }
 </style>
