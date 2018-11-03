5c737ee4f827cfb2547341350e9dc98a2f8536ff
Frank Matranga <thefrankmatranga@gmail.com>
2018-11-03 10:52:50 -0400

    404 route

--- src/router.js
+++ src/router.js
@@ -9,22 +9,26 @@ Vue.use(Router);
 const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('@/views/About.vue')
-    },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: () => import('@/views/Dashboard.vue')
-    }
+  routes: [{
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/About.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('@/views/Dashboard.vue')
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: () => import('@/views/NotFound.vue')
+  }
   ]
 });
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="notfound">
+    <section class="section">
+      <h1 class="title">Page Not Found</h1>
+      <div class="content">
+        <p>The page you are looking for does not exist! Trying checking the URL for typos.</p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang='scss' scoped>
+</style>
