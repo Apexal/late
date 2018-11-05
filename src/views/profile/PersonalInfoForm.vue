6bcb44cfbf1fb495018f253a4be12115ad299407
Frank Matranga <thefrankmatranga@gmail.com>
2018-11-05 12:01:35 -0500

    Personal info form work

--- src/views/profile/PersonalInfoForm.vue
+++ src/views/profile/PersonalInfoForm.vue
@@ -5,7 +5,7 @@
         <label class="label">First Name</label>
         <div class="control">
           <input
-            :value="user.name.first"
+            v-model="first_name"
             class="input"
             type="text"
             placeholder="First Name">
@@ -16,22 +16,54 @@
         <label class="label">Last Name</label>
         <div class="control">
           <input
-            :value="user.name.last"
+            v-model="last_name"
             class="input"
             type="text"
             placeholder="Last Name">
         </div>
       </div>
 
+      <div class="field">
+        <label class="label">RIN</label>
+        <div class="control">
+          <input
+            v-model="rin"
+            class="input"
+            type="text"
+            placeholder="RIN">
+        </div>
+      </div>
+
+      <button @click="save">Save</button>
     </form>
   </div>
 </template>
 
 <script>
+import API from '../../api';
+
 export default {
   name: 'PersonalInfoForm',
+  data () {
+    return {
+      first_name: '',
+      last_name: '',
+      rin: this.$store.state.auth.user.rin,
+      grad_year: this.$store.state.auth.user.grad_year
+    };
+  },
   computed: {
     user () { return this.$store.state.auth.user; }
+  },
+  methods: {
+    async save () {
+      const updatedUser = await API.post('/setup/personalinfo', {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        rin: this.rin,
+        grad_year: this.grad_year
+      });
+    }
   }
 };
 </script>
