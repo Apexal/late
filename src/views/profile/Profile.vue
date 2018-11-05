70091864a031ea1faa62eaa435b81d8ca5c500a1
Frank Matranga <thefrankmatranga@gmail.com>
2018-11-05 12:41:28 -0500

    Dynamic profile setup

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -2,6 +2,20 @@
   <div id="profile">
     <section class="section">
       <h1 class="title">Your Profile</h1>
+      <nav
+        class="breadcrumb"
+        aria-label="breadcrumbs">
+        <ul>
+          <li
+            v-for="(componentName, name) in setups"
+            :key="name"
+            :class="{ &quot;is-active&quot;: currentForm==componentName}">
+            <a
+              href="#"
+              @click="currentForm=componentName">{{ name }}</a>
+          </li>
+        </ul>
+      </nav>
       <keep-alive>
         <component :is="currentForm"/>
       </keep-alive>
@@ -15,9 +29,12 @@ import PersonalInfoForm from './PersonalInfoForm';
 export default {
   name: 'Profile',
   components: { PersonalInfoForm },
-  data () {
+  data() {
     return {
-      currentForm: 'PersonalInfoForm'
+      currentForm: '',
+      setups: {
+        'Personal Info': 'PersonalInfoForm'
+      }
     };
   }
 };
