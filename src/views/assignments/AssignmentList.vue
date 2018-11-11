f15a0bf225f35545e56792c40f0feef299bf80ba
Frank Matranga <thefrankmatranga@gmail.com>
2018-11-11 15:55:36 -0500

    Improved assignment list

--- src/views/assignments/AssignmentList.vue
+++ src/views/assignments/AssignmentList.vue
@@ -2,11 +2,11 @@
   <div class="assignment-list">
     <section class="section">
       <h1 class="title">All Assignments</h1>
-      <div class="upcoming-assignments">
+      <div class="upcoming-assignments columns is-multiline">
         <div
           v-for="(assignments, date) in assignmentsGroupedByDueDate"
           :key="date"
-          class="due-date"
+          class="due-date column is-one-third is-half-tablet"
         >
           <h3 class="subtitle is-4">{{ toDateShortString(date) }}</h3>
           <div
@@ -14,26 +14,30 @@
             :key="a._id"
             class="box"
           >
-            <b class="assignment-title">
-              <router-link :to="{ name: 'assignment-overview', params: { assignmentID: a._id }}">{{ a.title }}</router-link>
-              <span
-                v-if="a.priority >= 7"
-                class="tag is-danger"
-                title="You marked this assignment as high priority!"
-              >!</span>
-            </b>
-            <span
-              :title="toFullDateTimeString(a.dueDate)"
-              class="is-pulled-right"
-            >due {{ toTimeString(a.dueDate) }}</span>
-            <div
-              :title="toFullDateTimeString(a.dueDate)"
-              class="content"
-            >
-              <blockquote>
-                <p>{{ a.description }}</p>
-              </blockquote>
-            </div>
+            <details>
+              <summary>
+
+                <b class="assignment-title">
+                  <small class="course-title">{{ course(a).longname }}</small>
+                  <router-link :to="{ name: 'assignment-overview', params: { assignmentID: a._id }}">{{ a.title }}</router-link>
+                  <span
+                    v-if="a.priority >= 7"
+                    class="tag is-danger"
+                    title="You marked this assignment as high priority!"
+                  >!</span>
+                </b>
+                <span
+                  :title="toFullDateTimeString(a.dueDate)"
+                  class="is-pulled-right"
+                >due {{ toTimeString(a.dueDate) }}</span>
+              </summary>
+
+              <div class="content">
+                <blockquote>
+                  <p>{{ a.description }}</p>
+                </blockquote>
+              </div>
+            </details>
           </div>
           <hr>
         </div>
@@ -69,10 +73,18 @@ export default {
     },
     toTimeString (dueDate) {
       return moment(dueDate).format('hh:mm a');
+    },
+    course (a) {
+      return this.$store.getters.getCourseFromCRN(a.courseCRN);
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.assignment-title {
+  .course-title {
+    margin-right: 10px;
+  }
+}
 </style>
