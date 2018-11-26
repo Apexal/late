fae09c37b5ab413e61dc2ebda728c6a929353b7e
Frank Matranga <thefrankmatranga@gmail.com>
2018-11-26 16:36:37 -0500

    Improve Schedule sidebar

--- src/components/sidebar/Schedule.vue
+++ src/components/sidebar/Schedule.vue
@@ -6,8 +6,9 @@
     <summary class="panel-heading is-unselectable is-size-6">Today's Schedule
       <span
         v-if="in_class"
-        class="tag is-info"
-      >In Class</span>
+        class="tag is-info tooltip is-pulled-right"
+        :data-tooltip="'Until end of ' + current_course.longname + ' ' + periodType(current_period)"
+      >{{ countdown }}</span>
     </summary>
     <template v-if="is_weekend">
       <div class="panel-block">
@@ -24,18 +25,21 @@
         v-for="p in periods"
         :key="p.start"
         class="panel-block period-block is-clearfix is-size-7"
-        :class="{ 'is-active': p == current_period, 'has-background-white-ter': hasPassed(p) }"
+        :class="{ 'is-active': p == current_period, 'has-background-grey-lighter': hasPassed(p) }"
       >
         <span
-          class="course-longname is-full-width"
-          :title="periodType(p)"
+          class="course-longname is-full-width tooltip is-tooltip-bottom"
+          :data-tooltip="fromNow(p.start)"
         >
           <span
             class="dot"
             :style="'background-color: ' + course(p).color"
           />
-          {{ course(p).longname }} {{ periodType(p) }}
-          <div class="course-times is-pulled-right has-text-grey">
+          {{ course(p).longname }} <span class="has-text-grey">{{ periodType(p) }}</span>
+          <div
+            class="course-times is-pulled-right has-text-grey tooltip"
+            :data-tooltip="duration(p) + ' minutes'"
+          >
             <span>{{ timeFormat(p.start) }}</span>
             <span>{{ timeFormat(p.end) }}</span>
           </div>
@@ -51,7 +55,9 @@ import moment from 'moment';
 export default {
   name: 'Schedule',
   data () {
-    return {};
+    return {
+      now: new Date()
+    };
   },
   computed: {
     schedule () {
@@ -81,11 +87,23 @@ export default {
     },
     dateStr () {
       return moment(this.schedule.date).format('YYYY-MM-DD');
+    },
+    countdown () {
+      const diff = moment.duration(moment(this.current_period.end, 'Hmm', true).diff(this.now));
+      return `${diff.hours()}h ${diff.minutes()}m left`;
     }
   },
+  created () {
+    setInterval(() => { this.now = new Date(); }, 60 * 1000);
+  },
   methods: {
+    fromNow (datetime) {
+      const time = moment(datetime, 'Hmm', true);
+      return `${time.isBefore(this.now) ? 'Started' : 'Starting'} ${time.fromNow()}`;
+    },
     timeFormat: datetime => moment(datetime, 'Hmm', true).format('h:mma'),
     hasPassed: p => moment(p.end, 'Hmm', true).isBefore(moment()),
+    duration: p => moment(p.end, 'Hmm', true).diff(moment(p.start, 'Hmm', true), 'minutes'),
     course (p) {
       return this.$store.getters.getCourseFromPeriod(p);
     },
@@ -102,6 +120,7 @@ export default {
 }
 
 .course-times {
+  line-height: 11px;
   font-size: 12px;
   display: flex;
   flex-direction: column;
