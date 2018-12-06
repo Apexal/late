b8b5332483a47aef89446c5edaf919fb85facd0d
Zach <abc811@gmail.com>
2018-12-06 02:28:50 -0500

    Fixed a typo on the homepage
    Oops.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,13 +17,13 @@
             <li>automatically allocate study/work time throughout your schedule</li>
             <li>allow you to adjust the personal schedule made for you each week</li>
             <li>readjust itself and your calendar if you fall off track</li>
-            <li><span class="has-text-grey">(optionally)</span>keep you updated on your work and progress with weekly progress emails</li>
+            <li><span class="has-text-grey">(optionally)</span> keep you updated on your work and progress with weekly progress emails</li>
             <li><span class="has-text-grey">(optionally)</span> interact with you through text messages to remind you to study/work at the proper times</li>
             <li><span class="has-text-grey">(optionally)</span> interact with you through a variety of integrations including Discord and Slack</li>
           </ul>
         </p>
         <p>
-          <b>LATE</b> is a free, open-source project made by RPI students for RPI students only. It integrates with YACS and SIS to be much more than a
+          <b>LATE</b> is a free, open-source project made by RPI students for RPI students only. It integrates with SIS to be much more than a
           personal scheduling assistant.
         </p>
       </div>
