7c39ce834b0aa206f62374981d32b2c590e1aa28
Frank Matranga <thefrankmatranga@gmail.com>
2018-12-09 16:28:43 -0500

    cleaning up

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   lintOnSave: true,
+
   devServer: {
     proxy: 'http://localhost:3000'
+  },
+
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+
+  css: {
+    sourceMap: true
   }
 };
