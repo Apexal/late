04a554b459f71cb6095ac55e17c23bfe514c3165
Frank Matranga <thefrankmatranga@gmail.com>
2018-12-10 13:17:28 -0500

    Basic terms.js

---
+++ src/terms.js
@@ -0,0 +1,31 @@
+const moment = require('moment');
+/* Info from https://info.rpi.edu/registrar/academic-calendar */
+
+const list = [
+  {
+    name: 'Fall 2018',
+    code: '201809',
+    start: moment('2018-08-30', 'YYYY-MM-DD', true),
+    end: moment('2018-12-22', 'YYYY-MM-DD', true),
+    isBreak: false
+  },
+  {
+    name: 'Winter Break',
+    code: '201812',
+    start: moment('2018-12-23', 'YYYY-MM-DD', true),
+    end: moment('2019-01-08', 'YYYY-MM-DD', true),
+    isBreak: true
+  },
+  {
+    name: 'Spring 2019',
+    code: '',
+    start: moment('2019-01-09', 'YYYY-MM-DD', true),
+    end: moment('2019-05-04', 'YYYY-MM-DD', true),
+    isBreak: false
+  }
+];
+
+module.exports = {
+  current: () => list.find(t => moment().isBetween(t.start, t.end)) || null,
+  list
+};
