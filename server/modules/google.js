506e6c74fe8bf174552ad54d9ed238c6bfc0c8e2
Frank Matranga <thefrankmatranga@gmail.com>
2018-12-17 10:00:49 -0500

    Goole Auth works

--- server/modules/google.js
+++ server/modules/google.js
@@ -1,16 +1,23 @@
 const { google } = require('googleapis');
 
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URL
-);
+function createConnection () {
+  return new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URL
+  );
+}
 
+
+function createUrl (auth) {
+  return auth.generateAuthUrl({
+    scope: scopes
+  });
+}
 const scopes = ['https://www.googleapis.com/auth/calendar.events', 'https://www.googleapis.com/auth/calendar.settings.readonly'];
 
-const url = oauth2Client.generateAuthUrl({
-  scope: scopes
-});
 module.exports = {
-  url
+  apis: google,
+  createConnection,
+  createUrl
 };
