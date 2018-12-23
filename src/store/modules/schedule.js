682bed1916cfd064dacbece160e35a5d84e3d73c
Frank Matranga <thefrankmatranga@gmail.com>
2018-12-23 14:14:24 -0500

    Fixed inClass getter

--- src/store/modules/schedule.js
+++ src/store/modules/schedule.js
@@ -30,7 +30,7 @@ const getters = {
     return tms.find(t => moment(t.start).isAfter(moment()));
   },
   onBreak: (state, getters) => Object.keys(getters.currentTerm).length === 0,
-  inClass: state => !!state.current.period,
+  inClass: state => Object.keys(state.current.period).length !== 0,
   classesOver: (state, getters) => {
     return moment().isAfter(getters.currentTerm.classesEnd);
   },
