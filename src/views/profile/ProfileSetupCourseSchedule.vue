d1557a8d6f1ba357a892ac87c0ea72524d94add2
Frank Matranga <thefrankmatranga@gmail.com>
2018-12-23 23:42:19 -0500

    Break block setup

--- src/views/profile/ProfileSetupCourseSchedule.vue
+++ src/views/profile/ProfileSetupCourseSchedule.vue
@@ -1,146 +1,156 @@
 <template>
   <div class="setup-course-schedule">
-    <h2 class="is-size-4 integration-note">
-      What is your current semester schedule?
-    </h2>
-    <form
-      class="box"
-      @submit.prevent="save"
-    >
-      <details :open="crns.length === 0">
-        <summary>
-          <h2
-            style="display: inline-block; cursor: pointer;"
-            class="subtitle is-unselectable"
-          >
-            Automatically Set Your Course Schedule
-          </h2>
-        </summary>
-
-        <div class="columns">
-          <div class="field column is-narrow">
-            <label
-              for="method"
-              class="label"
-            >
-              Method
-            </label>
-            <div class="control">
-              <select
-                id="method"
-                v-model="method"
-                name="method"
-                class="control"
-              >
-                <option value="sis">
-                  SIS
-                </option>
-                <option value="crn">
-                  CRNs
-                </option>
-              </select>
-            </div>
-          </div>
-
-          <div
-            v-if="method == 'sis'"
-            class="sis-method column"
-          >
-            <div class="field">
-              <label
-                for="pin"
-                class="label"
-              >
-                SIS PIN
-              </label>
-              <p
-                class="help"
-              >
-                Your password will be used to log into SIS, navigate to your current schedule page, and grab the CRNs of your courses. Your password is never saved or logged anywhere.
-              </p>
-              <div class="control">
-                <input
-                  id="pin"
-                  v-model.trim="pin"
-                  type="password"
-                  class="input is-small"
-                  placeholder="Enter your SIS password."
-                  @change="saved = false"
-                >
-              </div>
-            </div>
-          </div>
-          <div
-            v-else-if="method == 'crn'"
-            class="crn-method column"
-          >
-            <div class="field">
-              <label
-                class="label"
-                for="crns"
-              >
-                Directly Enter Your Course CRNs
-              </label>
-              <p class="help">
-                These are found in SIS under 'View Weekly Schedule'.
-              </p>
-              <div class="control">
-                <input
-                  id="crns"
-                  v-model.trim="crns"
-                  class="input"
-                  name="crns"
-                  type="text"
-                  placeholder="123456, 654321, ..."
-                  @change="saved = false"
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <button
-          class="button is-small is-warning"
-          :class="{'is-loading': loading}"
-          :disabled="!canReset"
-        >
-          {{ user.setup.personal_info ? 'Reset Schedule' : 'Save' }}
-        </button>
-      </details>
-    </form>
-
-    <hr>
-
-    <p
-      v-if="courses.length === 0"
-      class="has-text-grey has-text-centered"
-    >
-      Set your courses above.
-    </p>
-    <template v-else>
-      <h2 class="subtitle">
-        Your Courses
+    <template v-if="onBreak">
+      <h2
+        class="subtitle has-text-centered"
+      >
+        You will be able to set your new course schedule once break ends.
       </h2>
-      <div class="columns is-multiline course-list">
-        <div
-          v-for="c in courses"
-          :key="c.crn"
-          class="column is-half"
-        >
-          <ProfileCourse
-            :course="c"
-            @update-course="updatedCourse"
-          />
-        </div>
-      </div>
     </template>
-    <router-link
-      to="/profile/unavailability"
-      class="button is-primary"
-      :class="{'is-loading': loading}"
-    >
-      Save and Continue
-    </router-link>
+    <template v-else>
+      <h2 class="is-size-4 integration-note">
+        What is your
+        <b>{{ currentTerm.name }}</b> schedule?
+      </h2>
+      <form
+        class="box"
+        @submit.prevent="save"
+      >
+        <details :open="crns.length === 0">
+          <summary>
+            <h2
+              style="display: inline-block; cursor: pointer;"
+              class="subtitle is-unselectable"
+            >
+              Automatically Set Your Course Schedule
+            </h2>
+          </summary>
+
+          <div class="columns">
+            <div class="field column is-narrow">
+              <label
+                for="method"
+                class="label"
+              >
+                Method
+              </label>
+              <div class="control">
+                <select
+                  id="method"
+                  v-model="method"
+                  name="method"
+                  class="control"
+                >
+                  <option value="sis">
+                    SIS
+                  </option>
+                  <option value="crn">
+                    CRNs
+                  </option>
+                </select>
+              </div>
+            </div>
+
+            <div
+              v-if="method == 'sis'"
+              class="sis-method column"
+            >
+              <div class="field">
+                <label
+                  for="pin"
+                  class="label"
+                >
+                  SIS PIN
+                </label>
+                <p
+                  class="help"
+                >
+                  Your password will be used to log into SIS, navigate to your current schedule page, and grab the CRNs of your courses. Your password is never saved or logged anywhere.
+                </p>
+                <div class="control">
+                  <input
+                    id="pin"
+                    v-model.trim="pin"
+                    type="password"
+                    class="input is-small"
+                    placeholder="Enter your SIS password."
+                    @change="saved = false"
+                  >
+                </div>
+              </div>
+            </div>
+            <div
+              v-else-if="method == 'crn'"
+              class="crn-method column"
+            >
+              <div class="field">
+                <label
+                  class="label"
+                  for="crns"
+                >
+                  Directly Enter Your Course CRNs
+                </label>
+                <p class="help">
+                  These are found in SIS under 'View Weekly Schedule'.
+                </p>
+                <div class="control">
+                  <input
+                    id="crns"
+                    v-model.trim="crns"
+                    class="input"
+                    name="crns"
+                    type="text"
+                    placeholder="123456, 654321, ..."
+                    @change="saved = false"
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <button
+            class="button is-small is-warning"
+            :class="{'is-loading': loading}"
+            :disabled="!canReset"
+          >
+            {{ user.setup.personal_info ? 'Reset Schedule' : 'Save' }}
+          </button>
+        </details>
+      </form>
+
+      <hr>
+
+      <p
+        v-if="courses.length === 0"
+        class="has-text-grey has-text-centered"
+      >
+        Set your courses above.
+      </p>
+      <template v-else>
+        <h2 class="subtitle">
+          Your Courses
+        </h2>
+        <div class="columns is-multiline course-list">
+          <div
+            v-for="c in courses"
+            :key="c.crn"
+            class="column is-half"
+          >
+            <ProfileCourse
+              :course="c"
+              @update-course="updatedCourse"
+            />
+          </div>
+        </div>
+      </template>
+      <router-link
+        to="/profile/unavailability"
+        class="button is-primary"
+        :class="{'is-loading': loading}"
+      >
+        Save and Continue
+      </router-link>
+    </template>
   </div>
 </template>
 
@@ -160,6 +170,12 @@ export default {
     };
   },
   computed: {
+    currentTerm () {
+      return this.$store.getters.currentTerm;
+    },
+    onBreak () {
+      return this.$store.getters.onBreak;
+    },
     canReset () {
       return !(this.pin.length === 0 && this.crns.length === 0);
     },
--- src/views/profile/ProfileSetupUnavailability.vue
+++ src/views/profile/ProfileSetupUnavailability.vue
@@ -1,85 +1,93 @@
 <template>
   <div class="setup-unavailability">
-    <h2
-      class="is-size-4 integration-note"
-    >
-      What times outside of class are you not able to study/work?
-    </h2>
-    <div class="box">
-      <form
-        id="time-preferences"
-        @submit.prevent="save"
+    <template v-if="onBreak">
+      <h2
+        class="subtitle has-text-centered"
       >
-        <div class="columns">
-          <div class="column field">
-            <label
-              for="earliest"
-              class="label"
-            >
-              Earliest Time Preference
-            </label>
-            <p class="help">
-              <b>LATE</b> will not schedule any work for you before this time unless it absolutely does not fit anywhere else.
-            </p>
-            <div class="control">
-              <input
-                id="earliest"
-                v-model="earliest"
-                min="00:00"
-                max="12:00"
-                type="time"
-                class="input is-small"
-                required
+        You will be able to set your new course schedule once break ends.
+      </h2>
+    </template>
+    <template v-else>
+      <h2 class="is-size-4 integration-note">
+        What times outside of class are you not able to study/work for
+        <b>{{ currentTerm.name }}</b>?
+      </h2>
+      <div class="box">
+        <form
+          id="time-preferences"
+          @submit.prevent="save"
+        >
+          <div class="columns">
+            <div class="column field">
+              <label
+                for="earliest"
+                class="label"
               >
+                Earliest Time Preference
+              </label>
+              <p class="help">
+                <b>LATE</b> will not schedule any work for you before this time unless it absolutely does not fit anywhere else.
+              </p>
+              <div class="control">
+                <input
+                  id="earliest"
+                  v-model="earliest"
+                  min="00:00"
+                  max="12:00"
+                  type="time"
+                  class="input is-small"
+                  required
+                >
+              </div>
+            </div>
+            <div class="column field">
+              <label
+                for="latest"
+                class="label"
+              >
+                Latest Time Preference
+              </label>
+              <p class="help">
+                <b>LATE</b> will not schedule any work for you after this time unless it absolutely does not fit anywhere else.
+              </p>
+              <div class="control">
+                <input
+                  id="latest"
+                  v-model="latest"
+                  type="time"
+                  class="input is-small"
+                  required
+                >
+              </div>
             </div>
           </div>
-          <div class="column field">
-            <label
-              for="latest"
-              class="label"
-            >
-              Latest Time Preference
-            </label>
-            <p class="help">
-              <b>LATE</b> will not schedule any work for you after this time unless it absolutely does not fit anywhere else.
-            </p>
-            <div class="control">
-              <input
-                id="latest"
-                v-model="latest"
-                type="time"
-                class="input is-small"
-                required
-              >
-            </div>
-          </div>
-        </div>
-      </form>
-    </div>
-    <h2 class="subtit">
-      Drag to set your study/work unavailability
-    </h2>
-    <p class="help">
-      Click on blocks to remove them. You can also drag, drop, and resive them.
-    </p>
-    <FullCalendar
-      ref="calendar"
-      :events="allEvents"
-      :editable="true"
-      :selectable="true"
-      :header="calendar.header"
-      :config="calendar.config"
-      @event-resize="eventResized"
-    />
-    <hr>
-    <button
-      form="time-preferences"
-      class="button is-primary"
-      :class="{'is-loading': loading}"
-      :disabled="saved"
-    >
-      Save and Continue
-    </button>
+        </form>
+      </div>
+      <h2 class="subtit">
+        Drag to set your study/work unavailability
+      </h2>
+      <p class="help">
+        Click on blocks to remove them. You can also drag, drop, and resive them.
+      </p>
+      <FullCalendar
+        ref="calendar"
+        :events="allEvents"
+        :editable="true"
+        :selectable="true"
+        :header="calendar.header"
+        :config="calendar.config"
+        @event-resize="eventResized"
+      />
+      <hr>
+      <button
+        form="time-preferences"
+        class="button is-primary"
+        :class="{'is-loading': loading}"
+        :disabled="saved"
+      >
+        Save and Continue
+      </button>
+    </template>
   </div>
 </template>
 
@@ -149,6 +157,12 @@ export default {
     };
   },
   computed: {
+    currentTerm () {
+      return this.$store.getters.currentTerm;
+    },
+    onBreak () {
+      return this.$store.getters.onBreak;
+    },
     allEvents () {
       return this.$store.getters.getCourseScheduleAsEvents.concat(
         this.calendar.events
