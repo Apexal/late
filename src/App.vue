d9afd6753e69f0e1bba3b3f2deb80a509f2d3e43
Frank Matranga <thefrankmatranga@gmail.com>
2018-12-31 19:50:27 -0500

    Fix

--- src/App.vue
+++ src/App.vue
@@ -138,7 +138,8 @@ export default {
       await this.$http.get('/students/loginas?rcs_id=' + rcsID);
     }
 
-    // await this.$store.dispatch('GET_USER');
+    // Seems like we need the following line no matter what :/
+    await this.$store.dispatch('GET_USER');
     if (this.$store.state.auth.isAuthenticated) {
       await this.$store.dispatch('GET_TERMS');
       await this.$store.dispatch('AUTO_UPDATE_SCHEDULE');
