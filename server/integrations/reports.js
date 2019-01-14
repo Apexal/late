7819ca35ea80de09b796abdb5aaa8ff6c9a323a5
Frank Matranga <thefrankmatranga@gmail.com>
2019-01-13 19:41:27 -0500

    Integration notification updates

--- server/integrations/reports.js
+++ server/integrations/reports.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('../db');
 
 const smsUtils = require('./sms').utils;
-const discordUtils = require('./discord').utils;
+const discord = require('./discord');
 const moment = require('moment');
 const logger = require('../modules/logger');
 
@@ -12,15 +12,16 @@ const Term = require('../api/terms/terms.model');
 const Block = require('../api/blocks/blocks.model');
 
 async function upcomingWorkBlockReminders () {
+  logger.info('Finding all upcoming work blocks to send reminders.');
   const terms = await Term.find().sort({ start: -1 });
   const currentTerm = terms.find(t => t.isCurrent);
 
-  // Globally find all work blocks starting within 15 minutes
-  const nowPlus30min = moment().add('30', 'minutes');
+  // Globally find all work blocks starting within 20 minutes
+  const nowPlus20min = moment().add('20', 'minutes');
   const upcomingWorkBlocks = await Block.find({
     startTime: {
       $gt: new Date(),
-      $lte: nowPlus30min
+      $lte: nowPlus20min
     },
     notified: false
   }).populate(
@@ -63,39 +64,22 @@ async function upcomingWorkBlockReminders () {
         assessment,
         block
       );
-    }
-  }
-}
-
-async function nightlyReport (integration = 'sms') {
-  const terms = await Term.find().sort({ start: -1 });
-  // Get all missed assignments
-  // Missed means (!a.completed && a.dueDate > moment().startOf('day) && a.dueDate < new Date())
-  const missedAssignments = await Assignment.getAllMissedAssignmentsForDay(
-    moment().startOf('day')
-  );
-  const students = [];
-
-  for (let a of missedAssignments) {
-    if (students.includes(a._student)) continue;
-    students.push(a._student);
-  }
-
-  for (let student of students) {
-    logger.info('Compiling nightly progress report to ' + student.rcs_id);
-    const missed = missedAssignments.filter(a => a._student === student);
-
-    if (integration === 'sms') {
-      smsUtils.generateNightlyReport(terms, student, missed);
-      logger.info('[Sent text]');
     } else if (integration === 'discord') {
-      discordUtils.sendNightlyReportMessage(terms, student, missed);
-      logger.info('[Sent Discord DM]');
+      await discord.utils.generateWorkBlockReminder(
+        discord.client,
+        terms,
+        block._student,
+        assessmentType,
+        assessment,
+        block
+      );
     }
+
+    block.notified = true;
+    await block.save();
   }
 }
 
 module.exports = {
-  upcomingWorkBlockReminders,
-  nightlyReport
+  upcomingWorkBlockReminders
 };
