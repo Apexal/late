cdc1973d7f1a74bab9fa97695f63072450536158
Abigail Medina <abigailmedina@rpi-wl-1222.dynamic.rpi.edu>
2019-01-29 17:50:20 -0500

    made it pretty

--- src/views/TheAboutPage.vue
+++ src/views/TheAboutPage.vue
@@ -2,55 +2,8 @@
   <div class="about">
     <section class="section">
       <h1 class="is-size-2 title">
-        Welcome to LATE
+        About Us
       </h1>
-      <div class=" column is-two-thirds">
-        <div class="columns about">
-          <img src="/availability.png">
-          <p class="right">
-            Tell us when you would like to work.
-          </p>
-        </div>
-
-        <div class="columns about">
-          <p class="left">
-            Easily add your RPI class schedule by letting us grab the information from SIS. No more manual schedule setting. Reimport your schedule when you add or drop a class and we take care of the rest.
-          </p>
-          <img src="/importSchedule.png">
-        </div>
-
-
-        <div class="columns about">
-          <p class="left">
-            Quickly be able to add, edit, and delete assignments for your classes.
-          </p>
-          <img src="/addAssignments.png">
-        </div>
-
-        <div class="columns about">
-          <img src="/upcomingAssignments.png">
-          <p class="right">
-            See a clear list of all upcoming assignments and tests.
-          </p>
-        </div>
-
-        <div class="columns about">
-          <img src="/dailyWorkSchedule.png">
-          <p class="right">
-            See a clear layout of your day! Just follow the list and let yourself be the lazy student you always dreamed of.
-          </p>
-        </div>
-
-        <div class="columns about">
-          <img src="/notify.png">
-          <p class="right">
-            We will notify you when it is time to work. You wont ever have to think about whether or not you <i>should</i> be working on something
-          </p>
-        </div>/
-      </div>
-
-
-      <!--
       <div class="columns">
         <div
           id="goal"
@@ -83,7 +36,7 @@
             </li>
           </ul>
         </div>
-      </div> -->
+      </div>
     </section>
   </div>
 </template>
@@ -114,28 +67,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .columns{
-    .left{
-      margin-left:-60px;
-      margin-right:2em !important;
-      text-align:right;
-    }
-    .right{
-      margin-right:-60px;
-      margin-left: 2em !important;
-    }
-    .about{
-      margin-bottom:8em;
-
-      p{
-        color: #4a4a4a;
-        font-weight: 400;
-        font-size: larger;
-      }
-      img{
-        width:75%;
-        height:75%;
-      }
-    }
-  }
 </style>
