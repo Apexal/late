a10ea0149cd313e733418af8b45e994b754fd2da
Frank Matranga <thefrankmatranga@gmail.com>
2019-03-15 13:05:04 -0400

    Format GCal event description

--- src/store/modules/gcal.js
+++ src/store/modules/gcal.js
@@ -1,13 +1,16 @@
 import axios from '@/api';
 
 const actions = {
-  async ADD_GCAL_EVENT_FOR_WORK_BLOCK ({ rootState }, { assessmentType, assessment, block }) {
+  async ADD_GCAL_EVENT_FOR_WORK_BLOCK ({ rootGetters, rootState }, { assessmentType, assessment, block }) {
     const assessmentURL = `https://rpi-late.herokuapp.com/${assessmentType + 's'}/${assessment._id}`;
+
+    const course = rootGetters.getCourseFromCRN(assessment.courseCRN);
+    const capitalizedAssessmentType = assessmentType === 'assignment' ? 'Assignment' : 'Exam';
 
     let request = await axios.post('/google/calendars/events', {
       calendarId: rootState.auth.user.integrations.google.calendarIDs.workBlocks,
       summary: `${assessmentType === 'assignment' ? 'Work on' : 'Study for'} ${assessment.title}`,
-      description: `View '${assessment.title}' at ${assessmentURL}.`,
+      description: `<b>${course.longname} ${capitalizedAssessmentType}</b> <i>${assessment.title}</i><br><br>${assessmentURL}`,
       source: {
         title: assessment.title,
         url: assessmentURL
