1da66a7e361fda40e437bc0a3322ca3fdf39c76d
Frank Matranga <thefrankmatranga@gmail.com>
2019-03-19 20:32:26 -0400

    Update addExamModal store

--- src/store/modules/addExamModal.js
+++ src/store/modules/addExamModal.js
@@ -2,22 +2,36 @@ const moment = require('moment');
 
 const state = {
   expanded: false,
-  date: moment().add(7, 'days'),
-  courseCRN: '00000'
+  courseCRN: '00000',
+  date: null,
+  time: '08:00',
+  title: '',
+  description: '',
+  timeEstimate: 5.0,
+  priority: 2
 };
-const getters = {
-  addExamModalDateString: state => state.date.format('YYYY-MM-DD'),
-  addExamModalTimeString: state => state.date.format('HH:mm')
+const actions = {
+  COPY_EXAM_TO_MODAL ({ commit }, assignment) {
+    commit('SET_ADD_ASSIGNMENT_MODAL_VALUES', {
+      courseCRN: assignment.courseCRN,
+      date: moment(assignment.date),
+      time: moment(assignment.date).format('HH:mm'),
+      title: assignment.title,
+      description: assignment.description,
+      timeEstimate: assignment.timeEstimate,
+      priority: assignment.priority
+    });
+  }
 };
 const mutations = {
-  SET_ADD_EXAM_MODAL_DATE: (state, date) => (state.date = date),
-  SET_ADD_EXAM_MODAL_COURSE_CRN: (state, courseCRN) =>
-    (state.courseCRN = courseCRN),
+  SET_ADD_EXAM_MODAL_VALUES: (state, updates) => {
+    return Object.assign(state, updates);
+  },
   TOGGLE_ADD_EXAM_MODAL: state => (state.expanded = !state.expanded)
 };
 
 export default {
   state,
-  getters,
+  actions,
   mutations
 };
