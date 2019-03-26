eb947e08e547ae0db7aaeb50fdc543f4c04cd919
AbigailMedina <AMEDINA2016@columbiasecondary.org>
2019-03-26 17:37:14 -0400

    two files converted, note TODOs

---
+++ src/views/assessments/AssessmentsCalendar.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="assessment-calendar">
+    <FullCalendar
+      ref="calendar"
+      :editable="false"
+      :selectable="false"
+      :header="calendar.header"
+      :config="calendar.config"
+    />
+  </div>
+</template>
+
+<script>
+import { FullCalendar } from 'vue-full-calendar';
+import 'fullcalendar/dist/fullcalendar.css';
+
+export default {
+  name: 'AssessmentsCalendar',
+  components: { FullCalendar },
+  props: {
+    showCompleted: {
+      type: Boolean,
+      default: true
+    },
+    filter: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      calendar: {
+        header: {
+          left: 'title',
+          center: '',
+          right: 'today prev,next'
+        },
+        config: {
+          validRange: {
+            start: this.$store.getters.currentTerm.start,
+            end: this.$store.getters.currentTerm.end
+          },
+          height: 800,
+          events: this.events,
+          defaultView: 'month',
+          timeFormat: 'h(:mm)t',
+          eventClick: (calEvent, jsEvent, view) => {
+            if (calEvent.assignment) {
+              this.$router.push(`/assignment/${calEvent.assignment._id}`);
+            } if (calEvent.exam) {
+              this.$router.push(`/exams/${calEvent.exam._id}`);
+            }
+          },
+          eventRender: (event) => {
+            if (
+              this.filter.includes(event.assignment.courseCRN) ||
+              (!this.showCompleted && event.assignment.completed) ||
+              (this.filter.includes(event.exam.courseCRN))
+            ) {
+              return false;
+            }
+          },
+          timezone: 'local',
+          dayClick: this.dayClick
+        }
+      }
+    };
+  },
+  watch: {
+    filter () {
+      this.$refs.calendar.fireMethod('rerenderEvents');
+    },
+    showCompleted () {
+      this.$refs.calendar.fireMethod('refetchEvents');
+    }
+  },
+  methods: {
+    async events (start, end, tz, callback) {
+      let request;
+      try {
+        request = await this.$http.get('/assignments', {
+          params: {
+            start: start.format('YYYY-MM-DD'),
+            end: end.format('YYYY-MM-DD')
+          }
+        });
+      } catch (e) {
+        this.events = [];
+        return this.$toasted.error(e.response.data.message);
+      }
+
+      const assignments = request.data.assignments;
+      const assignmentEvents = assignments
+        .filter(a => (this.showCompleted ? true : !a.completed))
+        .map(a => ({
+          title: a.title,
+          start: a.dueDate,
+          color: this.course(a).color,
+          assignment: a
+        }));
+      try {
+        request = await this.$http.get(
+          '/exams', { params: { start: start.format('YYYY-MM-DD'), end: end.format('YYYY-MM-DD') } }
+        );
+      } catch (e) {
+        this.events = [];
+        return this.$toasted.error(e.response.data.message);
+      }
+
+      const exams = request.data.exams;
+      const examEvents = exams
+        .map(e => ({
+          title: e.title,
+          start: e.date,
+          color: this.course(e).color,
+          exam: e
+        }));
+      const events = examEvents.concat(assignmentEvents);
+
+
+      callback(events);
+    },
+    dayClick (date) { // TODO added TOGGLE_ADD_ASSESSMENT_MODAL. add logic to receive this
+      this.$store.commit('SET_ADD_ASSIGNMENT_MODAL_VALUES', { dueDate: date });
+      this.$store.commit(
+        'TOGGLE_ADD_ASSESSMENT_MODAL'
+      );
+    },
+    course (a) {
+      return this.$store.getters.getCourseFromCRN(a.courseCRN);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
