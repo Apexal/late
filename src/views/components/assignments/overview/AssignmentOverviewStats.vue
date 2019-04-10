bf137fbb9b15e17917500b32d2cae93e6e992df0
Frank Matranga <thefrankmatranga@gmail.com>
2019-04-09 20:56:00 -0400

    Edit due date and time from AssignmentOverviewStats

--- src/views/components/assignments/overview/AssignmentOverviewStats.vue
+++ src/views/components/assignments/overview/AssignmentOverviewStats.vue
@@ -7,17 +7,18 @@
         </p>
         <p
           class="subtitle"
-          :title="'Priority level ' + assignment.priority"
-          :class="{ 'has-text-grey': assignment.priority === 1 }"
+          :class="{ 'is-italic': assignment.priority === 1 }"
           :style="{ 'font-weight': fontWeight }"
         >
           <i
             v-show="assignment.priority > 1"
             class="fas fa-minus has-text-grey decrease-priority"
+            :title="'Decrease priority to ' + priorityStrings[assignment.priority - 1]"
             @click="changePriority(-1)"
           /> {{ priorityString }} <i
             v-show="assignment.priority < 5"
             class="fas fa-plus has-text-grey increase-priority"
+            :title="'Increase priority to ' + priorityStrings[assignment.priority + 1]"
             @click="changePriority(1)"
           />
         </p>
@@ -29,11 +30,37 @@
         <p class="heading">
           {{ assignment.passed ? 'Was Due' : 'Due' }}
         </p>
+        <div
+          v-if="editingDueDate"
+          class="control"
+        >
+          <input
+            v-model="tempDueDateString"
+            :min="minDueDate"
+            :max="maxDueDate"
+            type="date"
+          >
+          <input
+            v-model="tempDueTimeString"
+            type="time"
+          >
+          <i
+            class="fas fa-check has-text-grey save-due-date"
+            title="Save new due date and time."
+            @click="updateDueDate"
+          />
+        </div>
         <p
-          class="subtitle tooltip"
+          v-else
+          class="subtitle tooltip is-tooltip-bottom"
           :data-tooltip="timeLeft"
         >
           {{ shortDueDateString }}
+          <i
+            class="fas fa-pencil-alt has-text-grey edit-due-date"
+            title="Edit due date and time."
+            @click="editingDueDate = true"
+          />
         </p>
       </div>
     </div>
@@ -46,7 +73,7 @@
           Completed
         </p>
         <p
-          class="subtitle tooltip"
+          class="subtitle tooltip is-tooltip-bottom"
           :data-tooltip="fromNow(assignment.completedAt)"
         >
           {{ completedAt }}
@@ -91,7 +118,24 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      loading: false,
+      editingDueDate: false,
+      tempDueDateString: '',
+      tempDueTimeString: ''
+    };
+  },
   computed: {
+    currentTerm () {
+      return this.$store.getters.currentTerm;
+    },
+    minDueDate () {
+      return moment(this.currentTerm.start).format('YYYY-MM-DD');
+    },
+    maxDueDate () {
+      return moment(this.currentTerm.end).format('YYYY-MM-DD');
+    },
     fontWeight () {
       return (
         {
@@ -103,16 +147,17 @@ export default {
         }[this.assignment.priority] || 600
       );
     },
+    priorityStrings () {
+      return {
+        1: 'Optional',
+        2: 'Low',
+        3: 'Normal',
+        4: 'High',
+        5: 'Important'
+      };
+    },
     priorityString () {
-      return (
-        {
-          1: 'Optional',
-          2: 'Low',
-          3: 'Normal',
-          4: 'High',
-          5: 'Important'
-        }[this.assignment.priority] || 'Unknown'
-      );
+      return this.priorityStrings[this.assignment.priority] || 'Unknown';
     },
     completedAt () {
       return moment(this.assignment.updatedAt).format('M/DD/YY h:mma');
@@ -123,21 +168,53 @@ export default {
       );
       return `${diff.days()}d ${diff.hours()}h ${diff.minutes()}m`;
     },
+    inputFormatDueDate () {
+      return moment(this.assignment.dueDate).format('YYYY-MM-DD');
+    },
+    inputFormatDueTime () {
+      return moment(this.assignment.dueDate).format('HH:mm');
+    },
     shortDueDateString () {
       return this.shortDateTimeString(this.assignment.dueDate);
     }
   },
+  watch: {
+    editingDueDate () {
+      this.tempDueDateString = this.inputFormatDueDate;
+      this.tempDueTimeString = this.inputFormatDueTime;
+    }
+  },
   methods: {
-    async changePriority (change) {
-      if (this.assignment.priority + change < 1 || this.assignment.priority + change > 5) { return; }
+    async updateDueDate () {
+      if (this.loading) return;
 
+      const newDueDate = moment(this.tempDueDateString + ' ' + this.tempDueTimeString, 'YYYY-MM-DD HH:mm', true);
+      if (moment(this.assignment.dueDate).isSame(newDueDate)) {
+        this.editingDueDate = false;
+        return;
+      }
+      if (newDueDate.isBefore(moment())) {
+        if (!confirm('Move this assignment to the past?')) return;
+      }
+      await this.updateAssignment({ dueDate: newDueDate.toDate() });
+
+      this.editingDueDate = false;
+    },
+    async changePriority (change) {
+      if (this.loading) return;
+
+      if (this.assignment.priority + change < 1 || this.assignment.priority + change > 5) { return; }
+      this.updateAssignment({ priority: this.assignment.priority + change });
+    },
+    async updateAssignment (updates) {
+      this.loading = true;
       let request;
       try {
         request = await this.$http.patch(
           `/assignments/a/${
             this.assignment._id
           }`,
-          { priority: this.assignment.priority + change }
+          updates
         );
       } catch (e) {
         this.$toasted.error(e.response.data.message);
@@ -159,6 +236,8 @@ export default {
           request.data.updatedAssignment
         );
       }
+      this.loading = false;
+      return request.data.updatedAssignment;
     },
     shortDateTimeString: dueDate =>
       moment(dueDate).format('ddd, MMM Do YY [@] h:mma'),
@@ -187,14 +266,16 @@ export default {
     color: white;
   }
 
-  .decrease-priority, .increase-priority {
+  .decrease-priority, .increase-priority, .edit-due-date, .save-due-date {
     cursor: pointer;
     opacity: 0;
     transition: opacity 0.2s;
   }
-
+  .save-due-date {
+    margin-left: 5px;
+  }
   &:hover {
-    .decrease-priority, .increase-priority {
+    .decrease-priority, .increase-priority, .edit-due-date, .save-due-date {
 
       opacity: 1;
     }
