8221db9b0daba2c4123511d70cb24de216875f73
Frank Matranga <thefrankmatranga@gmail.com>
2019-04-22 17:06:09 -0400

    FAB hover buttons for assessments

--- src/App.vue
+++ src/App.vue
@@ -84,12 +84,24 @@
           </div>
         </div>
 
-        <div
-          id="fab"
-          class="tooltip is-tooltip-left"
-          data-tooltip="Add new assignment or exam"
-        >
-          +
+        <div id="fab">
+          <p
+            class="option has-text-dark"
+            title="Add assignment"
+            @click="$store.commit('TOGGLE_ADD_ASSIGNMENT_MODAL')"
+          >
+            <i class="fas fa-clipboard-check" />
+          </p>
+          <p
+            class="option has-text-dark"
+            title="Add exam"
+            @click="$store.commit('TOGGLE_ADD_EXAM_MODAL')"
+          >
+            <i class="fas fa-exclamation-triangle" />
+          </p>
+          <div class="circle">
+            +
+          </div>
         </div>
       </template>
     </div>
@@ -220,27 +232,54 @@ export default {
 
 <style lang="scss">
 #fab {
+  display: flex;
   position: fixed;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   right: 10px;
   bottom: 10px;
-  width: 70px;
-  height: 70px;
   color: white;
-  z-index: 50;
-  border-radius: 100%;
-  font-size: 3em;
-
-  cursor: pointer;
-  transition: background-color 0.2s;
-  background-color: #222;
-
-  display: table-cell;
+  z-index: 10;
   text-align: center;
-  vertical-align: middle;
+  .option {
+    opacity: 0;
+    height: 0;
+    overflow: hidden;
+    transition: opacity 0.2s;
+    color: #222;
+    font-size: 2em;
+    width: 50px;
+
+    border-radius: 100%;
+
+    cursor: pointer;
+  }
+  .circle {
+    width: 60px;
+    height: 60px;
+    border-radius: 100%;
+    font-size: 3em;
+
+    cursor: pointer;
+    transition: background-color 0.2s;
+    background-color: #222;
+
+    line-height: 50px;
+    text-align: center;
+    vertical-align: middle;
+
+    &:hover {
+      box-shadow: 1px 1px 4px #444;
+      background-color: #333;
+    }
+  }
 
   &:hover {
-    box-shadow: 1px 1px 4px #444;
-    background-color: #333;
+    .option {
+      height: 50px;
+      opacity: 1;
+    }
   }
 }
 
