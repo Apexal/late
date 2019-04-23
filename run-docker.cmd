bfcf9e0e8e5c52b962250555c5f297b1e6f8a74b
Chris Nero <chrisvn00@gmail.com>
2019-04-23 14:53:29 -0400

    Start script for Windows

---
+++ run-docker.cmd
@@ -0,0 +1,9 @@
+:: Runs LATE in a docker container. This is meant for testing purposes,
+:: not necessarily for development.
+@echo off
+IF "%1" == "-build" (
+  docker build -t rcos/late .
+  :: delete stale Docker images if they have not been deleted already
+  docker rmi -f "$(docker images -q --filter 'dangling=true')"
+)
+docker run -p 3000:3000 rcos/late
