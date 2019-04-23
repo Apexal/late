14e5b02d9766a9d3c868b07067df2d43244c1c90
Chris Nero <chrisvn00@gmail.com>
2019-04-23 17:09:51 -0400

    Hopefully delete the stopped Docker container after running

--- run-docker.cmd
+++ run-docker.cmd
@@ -4,6 +4,8 @@
 IF "%1" == "-build" (
   docker build -t rcos/late .
   :: delete stale Docker images if they have not been deleted already
-  docker rmi -f "$(docker images -q --filter 'dangling=true')"
+  docker rmi $(docker images -q --filter 'dangling=true')
 )
-docker run -t -p 3000:3000 rcos/late
+docker run -it -p 3000:3000 rcos/late
+:: delete the container we just killed
+docker rm $(docker ps -aq --no-trunc)
