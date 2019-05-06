c8f59de2af283c6f458f67e191979f89850c610b
Frank Matranga <thefrankmatranga@gmail.com>
2019-05-06 02:33:47 -0400

    Start hidden GPA calculator

--- src/views/academicutils/AcademicUtils.vue
+++ src/views/academicutils/AcademicUtils.vue
@@ -69,13 +69,106 @@
         </div>
       </div>
     </div>
+
+    <!-- <div class="gpa-calculator box">
+      <form
+        class="form"
+        @submit.prevent="addCourse"
+      >
+        <input
+          v-model="newCourse.title"
+          type="text"
+          class="input"
+          required
+        >
+        <select v-model="newCourse.gradeValue">
+          <option
+            v-for="(value, letter) in grades"
+            :key="letter"
+            :value="value"
+          >
+            {{ letter }}
+          </option>
+        </select>
+        <input
+          v-model="newCourse.credits"
+          type="number"
+          min="1"
+          max="4"
+          step="1"
+          class="input"
+        >
+
+        <input
+          type="submit"
+          class="button"
+          value="Add"
+        >
+      </form>
+      <hr>
+      <ul>
+        <li
+v-for="c in courses"
+            :key="c.title"
+>
+          <b-tag type="is-success">
+            {{ c.gradeValue }}
+          </b-tag>
+          <b>{{ c.title }}</b> <i>{{ c.credits }}</i>
+        </li>
+      </ul>
+      <hr>
+      <h1>{{ gpa }}</h1>
+    </div> -->
   </section>
 </template>
 
 <script>
+const grades = {
+  'A': 4.0,
+  'A-': 3.7,
+  'B+': 3.3,
+  'B': 3.0,
+  'B-': 2.7,
+  'C+': 2.3,
+  'C': 2.0,
+  'C-': 1.7,
+  'D+': 1.3,
+  'D': 1.0,
+  'D-': 0.7,
+  'F': 0.0
+};
+
 export default {
   name: 'AcademicUtils',
+  data () {
+    return {
+      grades,
+      newCourse: {
+        title: '',
+        gradeValue: 4.0,
+        credits: 4
+      },
+      courses: []
+    };
+  },
+  computed: {
+    gpa () {
+      const total = this.courses.reduce((acc, course) => acc + course.gradeValue * course.credits, 0);
+      const takenCredits = this.courses.reduce((acc, course) => acc + course.credits, 0);
+
+      return total / takenCredits;
+    }
+  },
   methods: {
+    addCourse () {
+      this.courses.push(Object.assign({}, this.newCourse));
+      this.newCourse = {
+        title: '',
+        gradeValue: 4.0,
+        credits: 4
+      };
+    },
     launch (utilName) {
       this.$toast.open({
         message: 'Coming soon!',
