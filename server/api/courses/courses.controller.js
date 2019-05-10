049996a5ccf6b627d40eaa2cf23935f3067a8774
Frank Matranga <thefrankmatranga@gmail.com>
2019-05-10 10:40:00 -0400

    Update course API route

--- server/api/courses/courses.controller.js
+++ server/api/courses/courses.controller.js
@@ -17,6 +17,42 @@ async function getCourses (ctx) {
   return ctx.ok({ courses });
 }
 
+/**
+ * Update a course.
+ * Request body:
+ *  - any properties to patch the block with (excluding _id)
+ * @param {Koa context} ctx
+ */
+async function updateCourse (ctx) {
+  const { courseID } = ctx.params;
+
+  let course;
+  try {
+    course = await Course.findOne({
+      _id: courseID,
+      _student: ctx.state.user._id
+    });
+
+    // const forbiddenProperties = ['_id', '_student', 'crn', 'originalTitle'];
+    // if (forbiddenProperties.some(prop => ctx.request.body[prop] !== course[prop])) {
+    //   throw new Error('You cannot change the id, owner, crn, or original title of a course!');
+    // }
+
+    course.set(ctx.request.body);
+
+    await course.save();
+  } catch (e) {
+    logger.error(
+      `Failed to update course for ${ctx.state.user.rcs_id}: ${e}`
+    );
+    return ctx.badRequest('There was an error updating the course.');
+  }
+
+  logger.info(`Updated course for ${ctx.state.user.rcs_id}`);
+  return ctx.created({ updatedCourse: course });
+}
+
 module.exports = {
-  getCourses
+  getCourses,
+  updateCourse
 };
