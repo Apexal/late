5e044580d2b350043c24d4fd6bd09ce4b6049620
Frank Matranga <thefrankmatranga@gmail.com>
2019-05-20 02:00:29 -0400

    Create term API route

--- server/api/terms/terms.controller.js
+++ server/api/terms/terms.controller.js
@@ -1,4 +1,5 @@
 const logger = require('../../modules/logger');
+const moment = require('moment');
 
 const Term = require('./terms.model');
 
@@ -24,6 +25,50 @@ async function getTerms (ctx) {
   ctx.ok({ terms });
 }
 
+/**
+ * Create a new term.
+ *
+ * @param {Koa session} ctx
+ *
+ * GET /
+ **/
+async function createTerm (ctx) {
+  const { name, code, start, classesEnd, end } = ctx.request.body;
+
+  const createdTerm = new Term({
+    name,
+    code,
+    start,
+    classesEnd,
+    end
+  });
+
+  if (
+    moment(start).isSameOrBefore(end) ||
+    moment(classesEnd).isSameOrBefore(start) ||
+    moment(classesEnd).isAfter(end)
+  ) {
+    logger.error('Failed to add new term: Date are out of order.');
+    return ctx.badRequest('The date are out of order!');
+  }
+
+  try {
+    await createdTerm.save();
+  } catch (e) {
+    logger.error(
+      `Failed to get create term for admin ${ctx.state.user.rcs_id}: ${e}`
+    );
+    return ctx.internalServerError('There was an error creating the term.');
+  }
+
+  logger.info(
+    `Created term ${createdTerm.name} for admin ${ctx.state.user.rcs_id}.`
+  );
+
+  ctx.ok({ createdTerm });
+}
+
 module.exports = {
-  getTerms
+  getTerms,
+  createTerm
 };
--- server/api/terms/terms.model.js
+++ server/api/terms/terms.model.js
@@ -6,11 +6,10 @@ const moment = require('moment');
 const schema = new Schema(
   {
     name: { type: String, required: true },
-    code: { type: String, minlength: 6, maxlength: 6 },
+    code: { type: String, minlength: 6, maxlength: 6, required: true },
     start: { type: Date, required: true },
-    classesEnd: { type: Date },
-    end: { type: Date, required: true },
-    isBreak: { type: Boolean, default: false }
+    classesEnd: { type: Date, required: true },
+    end: { type: Date, required: true }
   },
   { timestamps: true }
 );
