b3e425ca986f695abcbc7864b8f3ed3b7e524813
Frank Matranga <thefrankmatranga@gmail.com>
2019-06-04 16:53:03 -0400

    Tools index page

--- src/views/tools/ToolsIndexPage.vue
+++ src/views/tools/ToolsIndexPage.vue
@@ -3,6 +3,65 @@
     <h1 class="title has-text-centered-mobile">
       Tools
     </h1>
+
+    <div class="tile is-ancestor">
+      <div class="tile">
+        <div class="tile is-parent is-vertical">
+          <router-link
+            tag="article"
+            :to="{ name: 'checklist' }"
+            class="tile is-child notification is-primary"
+          >
+            <p class="title">
+              Checklist
+            </p>
+            <p class="subtitle">
+              Create a checklist for all the items you need to move into your
+              dorm!
+            </p>
+          </router-link>
+          <router-link
+            tag="article"
+            :to="{ name: 'study-tools' }"
+            class="tile is-child notification is-dark"
+          >
+            <p class="title">
+              Study Timer
+            </p>
+            <p class="subtitle">
+              Work/study with the Pomodoro timer to keep you focused and
+              motivated!
+            </p>
+          </router-link>
+        </div>
+        <div class="tile is-parent is-vertical">
+          <router-link
+            tag="article"
+            :to="{ name: 'quick-links' }"
+            class="tile is-child notification is-link"
+          >
+            <p class="title">
+              Quick Links
+            </p>
+            <p class="subtitle">
+              Student curated list of useful RPI related links!
+            </p>
+          </router-link>
+          <router-link
+            tag="article"
+            :to="{ name: 'academic-utils' }"
+            class="tile is-child notification is-info"
+          >
+            <p class="title">
+              Grade Calculators
+            </p>
+            <p class="subtitle">
+              Calculate your GPA and estimate your course grades!
+            </p>
+          </router-link>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -13,4 +72,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.tile {
+  cursor: pointer;
+}
 </style>
