6776503524252ba2c5a8f9f3b275f29768a6287c
Frank Matranga <thefrankmatranga@gmail.com>
2019-06-19 01:56:34 -0400

    Properly setup service worker

--- vue.config.js
+++ vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
   pwa: {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
-      swSrc: 'src/sw.js'
+      swSrc: './src/service-worker.js'
     },
     name: 'LATE',
     themeColor: '#70cad1',
