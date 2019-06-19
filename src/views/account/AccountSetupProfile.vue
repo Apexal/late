e9764da9b2c890c3171256bbe8f14dfa7d5d30c6
Frank Matranga <thefrankmatranga@gmail.com>
2019-06-19 16:32:36 -0400

    Add major field and import from SIS button

--- src/views/account/AccountSetupProfile.vue
+++ src/views/account/AccountSetupProfile.vue
@@ -5,20 +5,44 @@
       Tell us about yourself!
     </h2>
     <form @submit.prevent="save">
-      <div class="field">
-        <label
-          class="label"
-          for="rcs-id"
-        >RCS ID</label>
-        <div class="control">
-          <input
-            id="rcs-id"
-            :value="user.rcs_id"
-            class="input"
-            type="text"
-            placeholder="RCS ID"
-            disabled
-          >
+      <div class="columns">
+        <div class="column is-half">
+          <div class="field">
+            <label
+              class="label"
+              for="rcs-id"
+            >RCS ID</label>
+            <div class="control">
+              <input
+                id="rcs-id"
+                :value="user.rcs_id"
+                class="input"
+                type="text"
+                placeholder="RCS ID"
+                disabled
+              >
+            </div>
+          </div>
+        </div>
+
+        <div class="column is-half">
+          <div class="field">
+            <label
+              class="label"
+              for="rin"
+            >RIN</label>
+            <div class="control">
+              <input
+                id="rin"
+                v-model.trim="rin"
+                class="input"
+                type="text"
+                placeholder="RIN"
+                required
+                @change="saved = false"
+              >
+            </div>
+          </div>
         </div>
       </div>
 
@@ -65,32 +89,12 @@
           </div>
         </div>
 
-        <div class="column is-half">
-          <div class="field">
-            <label
-              class="label"
-              for="rin"
-            >RIN</label>
-            <div class="control">
-              <input
-                id="rin"
-                v-model.trim="rin"
-                class="input"
-                type="text"
-                placeholder="RIN"
-                required
-                @change="saved = false"
-              >
-            </div>
-          </div>
-        </div>
-
         <div class="column">
           <div class="field">
             <label
               for="grad-year"
               class="label"
-            >Graduation Year</label>
+            >Graduation Year <small class="has-text-grey">(optional)</small></label>
             <div class="control">
               <input
                 id="grad-year"
@@ -107,18 +111,45 @@
             </div>
           </div>
         </div>
+
+        <div class="column">
+          <div class="field">
+            <label
+              for="major"
+              class="label"
+            >Major <small class="has-text-grey">(optional)</small></label>
+            <div class="control">
+              <input
+                id="major"
+                v-model.trim="major"
+                class="input"
+                type="text"
+                placeholder="What are you majoring in?"
+                required
+                @change="saved = false"
+              >
+            </div>
+          </div>
+        </div>
       </div>
       <hr>
 
-      <b-button
-        type="is-primary"
-        :loading="loading"
-        :disabled="saved"
-        class="is-pulled-right"
-        @click="save"
-      >
-        Save and Continue
-      </b-button>
+      <div class="buttons is-pulled-right">
+        <b-button
+          type="is-dark"
+          @click="startImportFromSIS"
+        >
+          Import from SIS
+        </b-button>
+        <b-button
+          type="is-primary"
+          :loading="loading"
+          :disabled="saved"
+          @click="save"
+        >
+          Save and Continue
+        </b-button>
+      </div>
     </form>
   </div>
 </template>
@@ -133,31 +164,98 @@ export default {
       first_name: '',
       last_name: '',
       rin: '',
-      grad_year: ''
+      grad_year: '',
+      major: ''
     };
   },
+  watch: {
+    user: 'updateFields'
+  },
   created () {
-    this.first_name = this.$store.state.auth.user.name
-      ? this.$store.state.auth.user.name.first
-      : '';
-    this.last_name = this.$store.state.auth.user.name
-      ? this.$store.state.auth.user.name.last
-      : '';
-
-    this.rin = this.$store.state.auth.user.rin || '';
-    this.grad_year = this.$store.state.auth.user.grad_year || '';
+    this.updateFields();
   },
   methods: {
+    updateFields () {
+      this.first_name = this.$store.state.auth.user.name
+        ? this.$store.state.auth.user.name.first
+        : '';
+      this.last_name = this.$store.state.auth.user.name
+        ? this.$store.state.auth.user.name.last
+        : '';
+
+      this.rin = this.$store.state.auth.user.rin || '';
+      this.grad_year = this.$store.state.auth.user.grad_year || '';
+      this.major = this.$store.state.auth.user.major || '';
+    },
+    promptRIN (onConfirm) {
+      this.$dialog.prompt({
+        message: 'What is your RPI RIN?',
+        inputAttrs: {
+          type: 'text',
+          minlength: 1,
+          maxlength: 20
+        },
+        onConfirm
+      });
+    },
+    promptPIN (onConfirm) {
+      this.$dialog.prompt({
+        message: 'What is your RPI SIS password?',
+        inputAttrs: {
+          type: 'password',
+          minlength: 1,
+          placeholder: 'This is not saved anywhere ever.',
+          maxlength: 300
+        },
+        onConfirm
+      });
+    },
+    startImportFromSIS () {
+      if (this.rin) {
+        this.promptPIN(pin => {
+          this.importFromSIS(this.rin, pin);
+        });
+      } else {
+        this.promptRIN(rin => {
+          this.promptPIN(pin => {
+            this.importFromSIS(rin, pin);
+          });
+        });
+      }
+    },
+    async importFromSIS (rin, pin) {
+      this.loading = true;
+      let request;
+      try {
+        request = await this.$http.post('/account/profile', {
+          method: 'sis',
+          rin,
+          pin
+        });
+      } catch (e) {
+        this.loading = false;
+        return this.$toast.open({
+          message: e.response.data.message,
+          type: 'is-danger'
+        });
+      }
+
+      await this.$store.dispatch('SET_USER', request.data.updatedUser);
+
+      this.loading = false;
+    },
     async save () {
       this.loading = true;
 
       let request;
       try {
         request = await this.$http.post('/account/profile', {
+          method: 'manual',
           first_name: this.first_name,
           last_name: this.last_name,
           rin: this.rin,
-          grad_year: this.grad_year
+          grad_year: this.grad_year,
+          major: this.major
         });
       } catch (e) {
         this.loading = false;
