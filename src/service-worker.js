69b611eb17c8279ce8660b2cf3c319aa2c0e7965
Frank Matranga <thefrankmatranga@gmail.com>
2019-06-19 02:10:25 -0400

    Cache academic calendar and announcements

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,13 @@
-console.log('Hello from sw.js');
+/* eslint-disable no-undef */
+
+/* The RPI academic calendar rarely changes... */
+workbox.routing.registerRoute(
+  '/api/integrations/academiccalendar',
+  new workbox.strategies.CacheFirst()
+);
+
+/* Announcements aren't the most pressing so they can update in the background */
+workbox.routing.registerRoute(
+  '/api/announcements',
+  new workbox.strategies.StaleWhileRevalidate()
+);
