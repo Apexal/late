d3696444d2e6594fb5b9e902735272b39df31b81
Frank Matranga <thefrankmatranga@gmail.com>
2019-07-09 19:16:24 -0400

    Proper socketio auth

--- server/modules/socketio.js
+++ server/modules/socketio.js
@@ -1,14 +1,9 @@
 const Student = require('../api/students/students.model');
 
+const logger = require('./logger');
+
 module.exports = server => {
-  let onlineCount = 0;
-  const messages = [
-    {
-      author: 'LATE',
-      text: 'Welcome!',
-      sentAt: new Date()
-    }
-  ];
+  let online = [];
 
   const io = require('socket.io')(server);
   require('socketio-auth')(io, {
@@ -18,6 +13,7 @@ module.exports = server => {
       Student.findById(studentID, function (err, user) {
         // inform the callback of auth success/failure
         if (err || !user) return callback(new Error('User not found'));
+        logger.info(`Authenticated socket for ${user.rcs_id}`);
         return callback(null, true);
       });
     },
@@ -25,31 +21,22 @@ module.exports = server => {
       Student.findById(studentID, function (err, user) {
         if (err) return console.log(err);
         socket.client.user = user;
+        online.push(user.rcs_id);
+        socket.emit('online', online);
       });
-    }
+    },
+    disconnect: function (socket) {
+      if (socket.client.user) { online = online.filter(rcsId => rcsId !== socket.client.user.rcs_id); }
+      socket.emit('online', online);
+    },
+    timeout: 10 * 1000
   });
 
   io.on('connection', socket => {
-    onlineCount += 1;
-    socket.emit('messages', messages);
-
-    socket.emit('online count', onlineCount);
-
     console.log('Client connected to Socket.io');
-
-    socket.on('get messages', () => {
-      socket.emit('messages', messages);
-    });
-
-    socket.on('message', message => {
-      messages.push({ sentAt: new Date(), ...message });
-      io.emit('messages', messages);
-    });
 
     socket.on('disconnect', () => {
       console.log('Client disconnected from Socket.io');
-      onlineCount -= 1;
-      socket.emit('online count', onlineCount);
     });
   });
 };
