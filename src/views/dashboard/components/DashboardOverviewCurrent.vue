8d44b4caf5f0b774e3074fc3d361f60e14f57e0b
Frank Matranga <thefrankmatranga@gmail.com>
2019-07-17 02:18:25 -0400

    DashboardOverviewCurrent start

---
+++ src/views/dashboard/components/DashboardOverviewCurrent.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="tile is-child notification is-dark dashboard-overview-current">
+    <p class="title">
+      Right Now
+    </p>
+
+    <p
+      v-if="currentStatus === 'busy'"
+      class="subtitle"
+    >
+      You are busy!
+    </p>
+    <div v-else-if="currentStatus === 'work-block'">
+      <p
+        class="subtitle"
+      >
+        For the next <b>{{ minutesLeft }} minutes</b> until <b>{{ timeFormat(currentEvent.end) }}</b>, you should be {{ currentEvent.assessment.assessmentType === 'assignment' ? 'working on' : 'studying for' }}
+      </p>
+      <router-link
+        class="has-text-centered is-size-2 assessment-link"
+        :to="assessmentRoute(currentEvent.assessment)"
+      >
+        <CourseAssessmentDot
+          :assessment="currentEvent.assessment"
+          :course="course(currentEvent.assessment)"
+        />
+        {{ currentEvent.assessment.title }}
+      </router-link>
+    </div>
+
+    <progress
+      class="progress is-small is-primary"
+      :value="percentThrough"
+      :title="percentThrough + '% through!'"
+      max="100"
+    >
+      {{ percentThrough }}%
+    </progress>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'DashboardOverviewCurrent',
+  computed: {
+    currentStatus () {
+      if (this.currentEvent) return this.currentEvent.eventType;
+      return 'busy';
+    },
+    todaysAgenda () {
+      return this.$store.getters.todaysAgenda;
+    },
+    currentEvent () {
+      return this.todaysAgenda.find(this.isCurrentEvent);
+    },
+    minutesLeft () {
+      return moment(this.currentEvent.end).diff(this.rightNow, 'minutes');
+    },
+    percentThrough () {
+      return Math.round((1 - this.minutesLeft / moment(this.currentEvent.end).diff(this.currentEvent.start, 'minutes')) * 100);
+    }
+  },
+  methods: {
+    isCurrentEvent (event) {
+      return moment(this.rightNow).isBetween(event.start, event.end);
+    },
+    assessmentRoute (assessment) {
+      return {
+        name: `${assessment.assessmentType}-overview`,
+        params: {
+          [`${assessment.assessmentType}ID`]: assessment._id
+        }
+      };
+    },
+    course (a) {
+      return this.$store.getters.getCourseFromCRN(a.courseCRN);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  margin-bottom: 0.25rem;
+}
+
+.progress {
+  margin-top: 1.5rem;
+}
+
+.assessment-link {
+  display: block;
+  text-align: center;
+  font-weight: bold;
+  text-decoration: none !important;
+}
+</style>
