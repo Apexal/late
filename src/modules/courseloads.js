b21349fe36bde1f4ecd1e52313d6d5cbd0da7502
Frank Matranga <thefrankmatranga@gmail.com>
2019-07-17 02:59:27 -0400

    Courseload estimator

--- src/modules/courseloads.js
+++ src/modules/courseloads.js
@@ -1,4 +1,15 @@
 export default {
+  determineWeight (assignmentCount, examCount) {
+    const weight = assignmentCount + examCount * 5;
+    if (weight === 0) {
+      return this.weights[0];
+    } else if (weight < 10) {
+      return this.weights[1];
+    } else if (weight < 15) {
+      return this.weight[2];
+    }
+    return this.weight[3];
+  },
   weights: [
     {
       className: 'is-white',
