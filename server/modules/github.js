64eca507fc9207b894641a376f1802328e92dc37
Frank Matranga <thefrankmatranga@gmail.com>
2019-08-15 13:42:47 -0400

    Add GitHub API and create issue method

---
+++ server/modules/github.js
@@ -0,0 +1,19 @@
+const Octokit = require('@octokit/rest');
+const octokit = new Octokit({
+  auth: process.env.GITHUB_ACCESS_TOKEN
+});
+
+const owner = 'apexal';
+const repo = 'late';
+
+module.exports = {
+  createIssue (submitterRCSID, title, body) {
+    return octokit.issues.create({
+      owner,
+      repo,
+      labels: ['Automated', 'User Submitted'],
+      title: title || 'User Submitted Issue',
+      body: body ? `**Issue submitted from the web app by ${submitterRCSID}**\n>${body}` : 'The use did not give any further info.'
+    });
+  }
+};
