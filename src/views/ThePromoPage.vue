39a5b410f296fee199ce29141c0708a885922ade
Frank Matranga <thefrankmatranga@gmail.com>
2019-08-19 01:53:19 -0400

    Complete Promo Page revamp - promos are now heros

--- src/views/ThePromoPage.vue
+++ src/views/ThePromoPage.vue
@@ -1,125 +1,130 @@
 <!-- Promo page-->
 <template>
-  <section
-    class="promo-container section"
-    style="margin-top: 24px"
-  >
-    <canvas id="confetti-canvas" />
-    <b-notification
-      v-if="waitlisted"
-      class="waitlist-notification"
-      type="is-info"
-    >
-      <b>WELCOME TO THE WAIT LIST</b> You are currently on the wait list and will be notified
-      by email once LATE opens to the student body. Reach out to the
-      <a href="mailto:matraf@rpi.edu">project lead</a> if you have any
-      questions, or join the
-      <a
-        target="_blank"
-        href="https://discord.gg/2GUKcHg"
-      >Discord server.</a>
-    </b-notification>
+  <div class="what-the">
     <div class="splash" />
-    <p id="sis-man-holder">
-      <img
-        title="Better LATE than never!"
-        src="@/assets/img/sisman.png"
-      >
-    </p>
-
-    <h1
-      class="title main-title"
-      style="text-align: center"
+    <section
+      class="promo-container section"
+      style="margin-top: 24px"
     >
-      Welcome to LATE
-    </h1>
-
-    <p class="tester-count has-text-grey has-text-centered">
-      <b-tag
-        type="is-dark"
-        class="counts"
-        size="is-medium"
+      <canvas id="confetti-canvas" />
+      <b-notification
+        v-if="waitlisted"
+        class="waitlist-notification"
+        type="is-info"
       >
-        <b>{{ testers }}</b> Current Testers |
-        <b>{{ waitlist }}</b>
-        {{ waitlist === 1 ? "Student" : "Students" }} on Wait List
-      </b-tag>
-    </p>
-    <div class="desc-container">
-      <p
-        class="is-size-8 has-text-centered desc"
-        style="max-width:800px;margin:0 auto;"
-      >
-        <b>LATE</b> is currently in closed BETA and is not available to the
-        general student body yet. Interested students can be added to the wait
-        list below and will be notified when we publicly launch in
-        <b>Fall 2019.</b>
-      </p>
-
-      <p class="has-text-centered">
+        <b>WELCOME TO THE WAIT LIST</b> You are currently on the wait list and will be notified
+        by email once LATE opens to the student body. Reach out to the
+        <a href="mailto:matraf@rpi.edu">project lead</a> if you have any
+        questions, or join the
         <a
-          href="/auth/login"
-          class="join-beta button is-primary is-large"
-          title="Log in to LATE beta"
-        >
-          <span class="icon">
-            <i class="fas fa-sign-in-alt" />
-          </span>
-          Join Waitlist</a>
-        <router-link
-          :to="{name: 'about'}"
-          class="the-other-button button is-secondary is-large"
-          title="What exactly is LATE?"
-        >
-          <span class="icon">
-            <i class="fas fa-info" />
-          </span>
+          target="_blank"
+          href="https://discord.gg/2GUKcHg"
+        >Discord server.</a>
+      </b-notification>
 
-          Learn More
-        </router-link>
-      </p>
-      <hr>
-      <div
-        class="columns is-multiline promos"
-        style="margin: 0 auto; max-width: 80vw;"
-      >
-        <div
-          v-for="(promo, index) in promos"
-          :key="index"
-          class="column is-one-third-fullhd is-half"
+      <p id="sis-man-holder">
+        <img
+          title="Better LATE than never!"
+          src="@/assets/img/sisman.png"
         >
-          <div
-            class="box is-unselectable promo"
-            @mouseenter="promoMouseEnter"
-            @mouseleave="promoMouseLeave"
+      </p>
+
+      <h1
+        class="title main-title"
+        style="text-align: center"
+      >
+        Welcome to LATE
+      </h1>
+
+      <p class="tester-count has-text-grey has-text-centered">
+        <b-tag
+          type="is-dark"
+          class="counts"
+          size="is-medium"
+        >
+          <b>{{ testers }}</b> Current Testers |
+          <b>{{ waitlist }}</b>
+          {{ waitlist === 1 ? "Student" : "Students" }} on Wait List
+        </b-tag>
+      </p>
+      <div class="desc-container">
+        <p
+          class="is-size-8 has-text-centered desc"
+          style="max-width:800px;margin:0 auto;"
+        >
+          <b>LATE</b> is currently in closed BETA and is not available to the
+          general student body yet. Interested students can be added to the wait
+          list below and will be notified when we publicly launch in
+          <b>Fall 2019.</b>
+        </p>
+
+        <p class="has-text-centered">
+          <a
+            href="/auth/login"
+            class="join-beta button is-primary is-large"
+            title="Log in to LATE beta"
           >
-            <h2 class="is-size-3 title">
-              {{ promo.title }}
-            </h2>
-            <h3 class="is-size-6 description">
-              {{ promo.description }}
-            </h3>
-            <img
-              v-if="promo.imageName"
-              class="example"
-              :src="'/img/promos/' + promo.imageName"
-            >
-            <div
-              v-else-if="promo.videoName"
-              class="example"
-            >
-              <video>
-                <source
-                  :src="'/video/promos/' + promo.videoName"
-                  type="video/mp4"
-                >
-              </video>
+            <span class="icon">
+              <i class="fas fa-sign-in-alt" />
+            </span>
+            Join Waitlist</a>
+          <button
+            class="the-other-button button is-secondary is-large"
+            title="What exactly is LATE?"
+            @click="scrollToPromos"
+          >
+            <span class="icon">
+              <i class="fas fa-chevron-down" />
+            </span>
+
+            Learn More
+          </button>
+        </p>
+      </div>
+    </section>
+    <section
+      v-for="(promo, index) in promos"
+      :id="'promo-' + index"
+      :key="index"
+      class="hero is-small promo"
+      :class="promo.type || 'is-primary'"
+      @mouseenter="promoMouseEnter"
+      @mouseleave="promoMouseLeave"
+    >
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-vcentered">
+            <div class="column">
+              <h1 class="title">
+                {{ promo.title }}
+              </h1>
+              <h2 class="subtitle">
+                {{ promo.description }}
+              </h2>
+            </div>
+            <div class="column">
+              <img
+                v-if="promo.imageName"
+                class="example is-pulled-right"
+                :src="'/img/promos/' + promo.imageName"
+              >
+              <div
+                v-else-if="promo.videoName"
+                class="example is-pulled-right"
+              >
+                <video>
+                  <source
+                    :src="'/video/promos/' + promo.videoName"
+                    type="video/mp4"
+                  >
+                </video>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -134,34 +139,40 @@ export default {
       waitlist: 0,
       promos: [
         {
+          type: 'is-primary',
           title: 'Login with your RPI account',
-          description: 'No need to make an account! Plus, you don\'t need to tell us your name or major or course schedule! Let us grab that from SIS for you!',
+          description: 'No need to make an account! Plus, you don\'t need to tell us your name or major or course schedule! Let us grab that from SIS for you! Once that is done you can manually change and add any information we have on you.',
           videoName: 'SIS.mp4'
         },
         {
+          type: 'is-info',
           title: 'Manage your entire courseload',
-          description: 'Just tell LATE what assignments and tests you have and it will handle the rest. You\'ll always have a clear overview of everything you need to do.',
+          description: 'Just tell LATE what assignments and tests you have and it will handle the rest. You\'ll always have a clear overview of everything you need to do. View your upcoming work in clear categories, in calendar form, or in list form. View statistics on your progress and study/work activities.',
           videoName: 'coursework.mp4'
         },
         {
+          type: 'is-link',
           title: 'Get notified to study/work',
-          description: 'Connect to SMS, Discord, Google Calendar, and more to receive reminders and manage your courseload. Chat with our bots to manage your work.',
+          description: 'Connect to SMS, Discord, Google Calendar, and more to receive reminders and manage your courseload. Chat with our bots to manage your work. Customize when you want to be reached out to and when you want to receive summaries of your progress along with recommendations.',
           videoName: 'reminders.mp4'
         },
         {
+          type: 'is-success',
           title: 'Collaborate with your peers',
-          description: 'Find students in your courses looking to form study groups, or create groups assignments to schedule group meetings to work. LATE will handle finding the rimes that work for everyone!',
+          description: 'Find students in your courses looking to form study groups, or create groups assignments to schedule group meetings to work. LATE will handle finding the times that work for everyone! No more trying to coordinate meeting up to study, let LATE do all the work for you.',
           videoName: 'collaborate.mp4'
         },
         {
+          type: 'is-warning',
           title: 'Use integrated student tools',
           description: 'Use LATE\'s grade calculators, work timers, and more tools which integrate with your courses and courseload. You don\'t even need to use LATE to use them!',
           videoName: 'tools.mp4'
         },
         {
+          type: 'is-danger',
           title: 'And much, much more!',
-          description: 'New features are constantly being added to LATE by the student team that works on it! We add features that solve the problems we encounter each day on campus.',
-          imageName: 'availability.png'
+          description: 'New features are constantly being added to LATE by the student team that works on it! We add features that solve the problems we encounter each day on campus. If you are a developer, contribute to the repo. If you are not, suggest new features directly!',
+          videoName: 'issues.mp4'
         }
       ]
     }
@@ -189,6 +200,11 @@ export default {
       this.testers = request.data.testers
       this.waitlist = request.data.waitlist
     },
+    scrollToPromos () {
+      document.getElementById('promo-0').scrollIntoView({
+        behavior: 'smooth'
+      })
+    },
     promoMouseEnter (e) {
       const videoEl = e.target.querySelector('video')
       if (!videoEl) return
@@ -214,6 +230,8 @@ export default {
 
 }
 .promo-container {
+  min-height: 95vh;
+
   padding: 0px !important;
   span.icon {
     margin-right: 0 !important;
@@ -224,12 +242,14 @@ export default {
   position: fixed;
   width: 110vw;
   left: 0;
-  top: 10px;
+  top: 50px;
   margin-left: -10vw;
   height: 500px;
   z-index: -1;
   background: url(/splash-bg.png);
   background-size: cover;
+  background-repeat-y: repeat;
+  height: 100%;
   //filter: blur(5px);
 }
 
@@ -283,13 +303,14 @@ export default {
   text-shadow: 0 1px 0 #52413c, 0px 0px 50px #52413c;
 }
 
-.desc-container {
-  margin-top: 10px;
-}
-
 .promo-container .desc-container {
+  margin: 0 auto;
+  margin-top: 10px;
   background-color: white;
-  padding: 20px 10px;
+  padding: 30px;
+  padding-top: 20px;
+  max-width: 800px;
+  border-radius: 6px;
 
   p.desc {
     font-size: 1.2em;
@@ -297,40 +318,35 @@ export default {
   }
 }
 
-.promos {
+.promo {
+  padding-top: 50px;
+  padding-bottom: 50px;
+  transition: 0.2s;
+  text-align: center;
+  //cursor:url('/img/icons/favicon.ico'), auto;
 
-  max-width: 90vw;
-  .promo {
-    transition: 0.2s;
-    text-align: center;
-    cursor: pointer;
-    //cursor:url('/img/icons/favicon.ico'), auto;
-
-    .example {
-      //border: 2px solid white;
-      border-radius: 6px;
-      margin-top: 10px;
-      overflow: hidden;
-
-      video {
-        filter: brightness(0.9);
-        transition: filter 0.5s;
-      }
-    }
-
-    &:hover {
-      .example video {
-        filter: brightness(1);
-      }
-    }
-
+  .example {
+    //border: 2px solid white;
+    border-radius: 6px;
+    margin-top: 10px;
+    overflow: hidden;
   }
 
-  .promo:hover {
-    box-shadow: 2px 3px 5px rgba(10, 10, 10, 0.3),
-      0 0 0 1px rgba(10, 10, 10, 0.1);
-
+  text-align: right;
+  &:nth-child(even) .columns {
+    flex-direction: row-reverse;
+    text-align: left;
   }
+
+  @media only screen and (max-width: 768px) {
+    .columns {
+      text-align: center !important;
+    }
+  }
+}
+
+.promo:last-child {
+  margin-bottom: -30px;
 }
 
 .counts {
