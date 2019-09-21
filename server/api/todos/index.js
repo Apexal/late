bc2e41213f0fdb3a5efc4ca17cd6de0c156f0a7f
Erik Roberts <erik@bugg.co>
2019-09-21 00:13:21 -0400

    Added basic todo persistence Added documentation to SidebarTodoList.vue See issue #386

--- server/api/todos/index.js
+++ server/api/todos/index.js
@@ -5,6 +5,7 @@ const Ctrl = require('./todos.controller')
 
 router.get('/', Ctrl.getTodos)
 router.post('/', Ctrl.createTodo)
+router.post('/:todoID', Ctrl.updateTodo)
 router.delete('/:todoID', Ctrl.deleteTodo)
 
 module.exports = router.routes()
