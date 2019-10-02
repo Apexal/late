4cc17ec5e5e839ac9dcd71c7b4565c1981c31f0e
Frank Matranga <thefrankmatranga@gmail.com>
2019-10-01 21:09:02 -0400

    Start assessment reminder component

---
+++ src/views/assessments/components/overview/AssessmentOverviewReminders.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="assessment-reminders">
+    Coming soon!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AssessmentOverviewReminders',
+  props: {
+    assessment: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
