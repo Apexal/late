05e5bab423ea5958e2839a8e47db7b7b9b9abb10
Aaron Reers <reersa@rpi.edu>
2019-10-04 15:49:32 -0400

    Created template page for the home page

--- src/views/studygroups/StudyGroupsHome.vue
+++ src/views/studygroups/StudyGroupsHome.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="study-groups-home">
-    <p>Home</p>
+    <p
+      v-if="!currentGroups.length"
+      class="has-text-centered has-text-grey no-groups"
+    >
+      No current study groups. Create or join a group to get studying!
+    </p>
+    <b-button
+      @click="currentGroups.push('group')"
+    >
+      Test
+    </b-button>
   </div>
 </template>
 <script>
 export default {
-  name: 'StudyGroupsHome'
+  name: 'StudyGroupsHome',
+  props: {
+    currentGroups: {
+      type: Array,
+      default: () => []
+    }
+  }
 }
 </script>
-<style>
+<style lang="scss" scoped>
+.no-groups {
+  padding-top: 50px;
+}
 </style>
