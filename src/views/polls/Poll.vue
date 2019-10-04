bbf01213b81c5e371ebe60c4042b9c42da0438aa
aidan-lane <aidanlane7@gmail.com>
2019-10-04 16:48:16 -0400

    Added base poll text code

---
+++ src/views/polls/Poll.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <VuePoll
+      v-bind="options"
+      @addvote="addVote"
+    />
+  </div>
+</template>
+
+<script>
+
+import VuePoll from 'vue-poll'
+
+export default {
+  components: {
+    VuePoll
+  },
+  data () {
+    return {
+      options: {
+        question: 'What\'s your favourite <strong>JS</strong> framework?',
+        answers: [
+          { value: 1, text: 'Vue', votes: 53 },
+          { value: 2, text: 'React', votes: 35 },
+          { value: 3, text: 'Angular', votes: 30 },
+          { value: 4, text: 'Other', votes: 10 }
+        ]
+      }
+    }
+  },
+  methods: {
+    addVote (obj) {
+      console.log('You voted ' + obj.value + '!')
+    }
+  }
+}
+</script>
