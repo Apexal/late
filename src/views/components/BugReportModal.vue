99fe00804afa5c94568cdc8997f732f9b2b68fb4
Tobias <tobiasjpark73@gmail.com>
2019-10-11 16:44:21 -0400

    Modal code written

--- src/views/components/BugReportModal.vue
+++ src/views/components/BugReportModal.vue
@@ -1,6 +1,30 @@
 <template>
-  <b-modal>
-    <!-- asjdlkfjalk -->
+  <b-modal
+    class="bug-report-modal"
+    style="height: 100%;"
+    :active.sync="bugReportModalOpen"
+  >
+    <form>
+      Enter a title for this issue: <br>
+      <input
+        type="text"
+        name="title"
+      > <br>
+      Describe the issue in as much detail as possible.<br>
+      <textarea
+        id="description"
+        name="description"
+      /><br>
+      Optional: Enter your RCS ID here so we can contact you for more information about this issue. (We may also give you credit for reporting this issue!)
+      <input
+        id="rcsid"
+        type="text"
+      >
+      <input
+        type="submit"
+        value="Submit"
+      >
+    </form>
   </b-modal>
 </template>
 
@@ -13,3 +37,5 @@ export default {
 <style>
 
 </style>
+
+<!--    -->
