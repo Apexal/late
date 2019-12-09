2be9b5f7af3dedda77825ea6314f604ac2ca5e80
Frank Matranga <thefrankmatranga@gmail.com>
2019-12-09 15:14:18 -0500

    Fix issues #487 #454 (#561)
    * Fixed Account Complete buttons and Dashboard Calendar fullscreen
    
     - Account Complete button animations now mirror Tools Index buttons
     - Set a maximum height for the Dashboard Calendar, which fixes cut-off issues in fullscreen
    
    * Fixed Account Complete buttons and Dashboard Calendar fullscreen
    
    Fixes issues #487 and #454
     - Account Complete button animations now mirror Tools Index buttons
     - Set a maximum height for the Dashboard Calendar, which fixes cut-off issues in fullscreen
    
    
    Co-authored-by: Zach <abc811@gmail.com>

--- src/views/dashboard/DashboardCalendar.vue
+++ src/views/dashboard/DashboardCalendar.vue
@@ -465,7 +465,11 @@ export default {
 }
 .exit-fullscreen {
   transition: 0.2s;
-  margin-top: -50px;
+  margin-top: -35px;
   .fa-expand-arrows-alt { display: none; }
+}
+
+.fc-time-grid-container {
+  max-height: 80vh!important;
 }
 </style>
