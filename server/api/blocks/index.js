66e6a0406c55f6c95b1fb23fd87bacc9be79900f
Frank Matranga <thefrankmatranga@gmail.com>
2019-12-23 14:05:17 -0500

    Todo block routes

--- server/api/blocks/index.js
+++ server/api/blocks/index.js
@@ -13,8 +13,12 @@ router.post('/course/:courseID', Ctrl.addCourseBlock)
 router.patch('/course/:courseID/:blockID', Ctrl.editCourseBlock)
 router.delete('/course/:courseID/:blockID', Ctrl.deleteCourseBlock)
 
-// To Do Blocks
+// Todo Blocks
+router.post('/todo/:todoID', Ctrl.addTodoBlock)
+router.patch('/todo/:todoID/:blockID', Ctrl.editTodoBlock)
+router.delete('/todo/:todoID/:blockID', Ctrl.deleteTodoBlock)
 
+// Assessment blocks
 router.post('/assessment/:assessmentType/:assessmentID', Ctrl.addAssessmentBlock)
 router.patch('/assessment/:assessmentType/:assessmentID/:blockID', Ctrl.editAssessmentBlock)
 router.delete('/assessment/:assessmentType/:assessmentID/:blockID', Ctrl.deleteAssessmentBlock)
