0492460ec41540e9a7ad58d9ed90798e30373d39
Frank Matranga <thefrankmatranga@gmail.com>
2020-01-17 10:15:07 -0500

    Fix date times

--- src/mixins/datemethods.js
+++ src/mixins/datemethods.js
@@ -7,7 +7,7 @@ const LONG_DATE_FORMAT = 'dddd, MMMM Do YYYY '
 
 export default {
   methods: {
-    timeFormat: dateOrStr => (typeof date === 'string' ? moment(dateOrStr) : moment(dateOrStr, 'HH:mm')).format(TIME_FORMAT),
+    timeFormat: dateOrStr => (dateOrStr.length === 5 ? moment(dateOrStr, 'HH:mm') : moment(dateOrStr)).format(TIME_FORMAT), // Very bad I know
     shortDateFormat: date => moment(date).format(SHORT_DATE_FORMAT),
     longDateFormat: date => moment(date).format(LONG_DATE_FORMAT),
     shortDateTimeFormat: date =>
