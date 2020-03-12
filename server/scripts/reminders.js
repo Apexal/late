7ceeaf9243de7234c7e61300c815cc1ae65066dc
Frank Matranga <thefrankmatranga@gmail.com>
2020-03-12 15:56:12 -0400

    Send reminders script start

--- server/scripts/reminders.js
+++ server/scripts/reminders.js
@@ -6,27 +6,76 @@ const moment = require('moment')
 require('../db')
 
 const Assignment = require('../api/assignments/assignments.model')
+const Exam = require('../api/exams/exams.model')
 
-async function sendReminder (assessment, assessmentType, reminder) {
-  if (assessmentType === 'assignment') {
+const { sendText } = require('../integrations/sms')
+const { sendGenericEmail } = require('../integrations/email')
 
-  } else if (assessmentType === 'exam') {
+async function sendReminder (assessment, reminder) {
+  const { assessmentType } = assessment
 
+  if (reminder.integration === 'sms') {
+    let message
+    if (assessmentType === 'assignment') {
+      message = `Just reminding you about your '${assessment.title}' assignment due ${assessment.dueDate}!`
+    } else if (assessmentType === 'exam') {
+      message = `Don't forget about your exam '${assessment.title}' on ${assessment.date}!`
+    }
+
+    try {
+      console.log('Sending reminder')
+      await sendText(assessment._student.integrations.sms.phoneNumber, message)
+      reminder.sent = true
+    } catch (e) {
+      console.error(e)
+    }
+  } else if (reminder.integration === 'email') {
+    try {
+      logger.info('Sending email reminder...')
+      await sendGenericEmail(assessment._student.rcs_id, 'Reminder', {
+        preheader: `An ${assessmentType} is coming up!`,
+        title: 'Title Woo!',
+        content: `
+        <p>
+          Don't forget about your <b>${assessmentType}</b> <i>${assessment.title}</i> coming up soon!
+        </p>
+        `,
+        buttons: [
+          {
+            text: 'View on LATE',
+            url: 'https://late.work/coursework/' + assessment.assessmentType.charAt(0) + '/' + assessment._id
+          }
+        ]
+      })
+      reminder.sent = true
+    } catch (e) {
+      console.error(e)
+    }
   }
-  reminder.sent = true
-  await reminder.save()
 }
 
-async function exams () {
+async function findAndSendAssessments () {
+  logger.info('Finding assessments with reminders to be sent...')
   const now = Date.now()
-  const max = moment(now).add(1, 'hour')
-  const assignments = await Assignment.find({ 'reminders.sent': false,
-    'reminders.datetime': {
-      $gte: now,
-      $lte: max
-    } })
 
-  console.log(assignments)
+  const query = {
+    'reminders.sent': false,
+    'reminders.datetime': {
+      $lte: now
+    }
+  }
+
+  const [assignments, exams] = await Promise.all([Assignment.find(query).populate('_student', 'rcs_id name integrations'), Exam.find(query).populate('_student', 'rcs_id name integrations')])
+
+  await Promise.all(assignments.concat(exams).map(async assessment => {
+    const reminders = assessment.reminders.filter(r => r.sent === false && r.datetime <= now)
+
+    await Promise.all(reminders.map(async reminder => sendReminder(assessment, reminder)))
+    return assessment.save()
+  }))
+
+  logger.info('Finished sending reminders... Exiting.')
+  process.exit()
 }
 
-exams()
+findAndSendAssessments()
