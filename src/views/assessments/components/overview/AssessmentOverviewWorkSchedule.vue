0d054b39b56853ff0735454d90eeb806078b040d
Frank Matranga <thefrankmatranga@gmail.com>
2020-04-29 01:00:48 -0400

    Proper schedule constraints

--- src/views/assessments/components/overview/AssessmentOverviewWorkSchedule.vue
+++ src/views/assessments/components/overview/AssessmentOverviewWorkSchedule.vue
@@ -60,6 +60,7 @@
       :views="calendar.views"
       :valid-range="validRange"
       :select-constraint="selectConstraint"
+      :event-constraint="selectConstraint"
       :business-hours="calendar.businessHours"
       :height="700"
       all-day-text="Due"
@@ -146,6 +147,10 @@ export default {
   },
   computed: {
     selectConstraint () {
+      // If assessment is a completed assignment, the completedAt date might be earlier and should be used
+      if (this.assessmentType === 'assignment' && this.assessment.completedAt && moment(this.assessment.completedAt).isBefore(this.assessmentDate)) {
+        return { end: this.assessment.completedAt }
+      }
       return { end: this.assessmentDate }
     },
     scheduledMinutes () {
