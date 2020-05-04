38bb406ba82e6d0c28547afbec5efabc84aeb1b8
Frank Matranga <thefrankmatranga@gmail.com>
2020-05-03 21:00:45 -0400

    Courseload calculator finds next week

--- src/views/dashboard/components/DashboardOverviewCourseload.vue
+++ src/views/dashboard/components/DashboardOverviewCourseload.vue
@@ -4,7 +4,7 @@
     :class="courseLoad.className"
   >
     <p class="title is-marginless">
-      Next 7-Day Courseload
+      Next Week's Courseload
     </p>
     <p class="is-size-2 course-load">
       {{ courseLoad.title }}
@@ -25,17 +25,29 @@ export default {
   name: 'DashboardOverviewCourseload',
   computed: {
     courseLoad () {
-      return courseloads.determineWeight(this.assignments.concat(this.exams))
+      return courseloads.determineWeight(this.upcomingWeekAssessments)
+    },
+    startDate () {
+      const start = moment(this.rightNow)
+      while (start.day() !== 0) {
+        alert('add 1 day')
+        start.add(1, 'day')
+      }
+      return start.startOf('day')
     },
     upcomingWeekAssessments () {
-      const limit = moment().add(7, 'days').endOf('day')
-      return this.$store.state.assessments.upcomingAssessments.filter(assessment => moment(assessment.date).isSameOrBefore(limit))
+      return this.$store.state.assessments.upcomingAssessments.filter(assessment => moment(assessment.date).isBetween(this.startDate, moment(this.startDate).add(1, 'week')))
     },
     assignments () {
       return this.upcomingWeekAssessments.filter(a => a.assessmentType === 'assignment')
     },
     exams () {
       return this.upcomingWeekAssessments.filter(a => a.assessmentType === 'exam')
+    }
+  },
+  watch: {
+    startDate2 (newDate) {
+      console.log(newDate)
     }
   }
 }
